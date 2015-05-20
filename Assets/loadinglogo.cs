4a9f06316eac2c52f41330e8287c8b12dfc470f9
Kevin Glaap <kevin.glaap@gmail.com>
2015-05-20 13:37:28 +0200

    Loading logo

---
+++ Assets/loadinglogo.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class loadinglogo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		disable ();
+	}
+	
+
+
+
+	public void disable(){
+
+		foreach (Image i in GetComponentsInChildren<Image>()) {
+
+			i.enabled = false;
+
+		}
+
+		foreach (Text t in GetComponentsInChildren<Text>()) {
+			
+			t.enabled = false;
+			
+		}
+
+	}
+
+
+
+	public void enable(){
+		
+		foreach (Image i in GetComponentsInChildren<Image>()) {
+			
+			i.enabled = true;
+			
+		}
+		
+		foreach (Text t in GetComponentsInChildren<Text>()) {
+			
+			t.enabled = true;
+			
+		}
+		
+	}
+}
---
+++ Assets/texttotext.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class texttotext : MonoBehaviour {
+
+
+
+public void setText(string s){
+
+		GetComponent<Text> ().text = s;
+
+	}
+}
