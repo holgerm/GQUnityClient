92855642b4d8ab024b5b5ec4c4999325dc433640
Kevin Glaap <kevin.glaap@gmail.com>
2015-06-02 14:33:14 +0200

    BackgroundImage in Multiple Choice Question + some design tweaks for new top menu

--- Assets/Scripts/page_multiplechoicequestion.cs
+++ Assets/Scripts/page_multiplechoicequestion.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 
 using System.Collections;
+using GQ.Util;
 
 public class page_multiplechoicequestion : MonoBehaviour {
 
@@ -20,53 +21,149 @@ public class page_multiplechoicequestion : MonoBehaviour {
 	public Text questiontext;
 	public VerticalLayoutGroup list;
 	public multiplechoiceanswerbutton answerbuttonprefab;
-	
+
+	public questdatabase questdb;
+	public actions 	questactions;
+		public Image image;
+
 	// Use this for initialization
 	void Start () {
-		quest = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().currentquest;
-		multiplechoicequestion = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().currentquest.currentpage;
 
 
-		if(multiplechoicequestion.onStart != null){
+		if (GameObject.Find ("QuestDatabase") == null) {
 			
-			multiplechoicequestion.onStart.Invoke();
-		}
+			Application.LoadLevel (0);
+			
+		} else {
+
+
+			questdb = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ();
+			quest = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().currentquest;
+			multiplechoicequestion = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().currentquest.currentpage;
+			questactions = GameObject.Find ("QuestDatabase").GetComponent<actions> ();
+
+
+			if (multiplechoicequestion.onStart != null) {
+			
+				multiplechoicequestion.onStart.Invoke ();
+			}
 
 
 
 
 
-		if (multiplechoicequestion.contents_question != null) {
+			if (multiplechoicequestion.contents_question != null) {
 
 
-			//multiplechoicequestion.contents_question.questiontext.
-
-
-
-				} else {
+				//multiplechoicequestion.contents_question.questiontext.
 
 
 
-						questiontext.text = multiplechoicequestion.getAttribute ("question");
-
-						foreach (QuestContent qc in multiplechoicequestion.contents_answers) {
+			} else {
 
 
-								multiplechoiceanswerbutton btn = (multiplechoiceanswerbutton)Instantiate (answerbuttonprefab, transform.position, Quaternion.identity);
-								btn.transform.SetParent (list.transform);
-								btn.transform.localScale = new Vector3 (1f, 1f, 1f);
-								btn.setText (qc.content);
 
-								if (qc.getAttribute ("correct") == "1") {
-										btn.correct = true; 
-								} else {
-										btn.correct = false;
+				questiontext.text = multiplechoicequestion.getAttribute ("question");
 
-								}
-						}
+				foreach (QuestContent qc in multiplechoicequestion.contents_answers) {
 
 
+					multiplechoiceanswerbutton btn = (multiplechoiceanswerbutton)Instantiate (answerbuttonprefab, transform.position, Quaternion.identity);
+					btn.transform.SetParent (list.transform);
+					btn.transform.localScale = new Vector3 (1f, 1f, 1f);
+					btn.setText (qc.content);
+
+					if (qc.getAttribute ("correct") == "1") {
+						btn.correct = true; 
+					} else {
+						btn.correct = false;
+
+					}
 				}
+
+
+			}
+
+
+
+
+
+
+
+			if (multiplechoicequestion.getAttribute ("bg") != "") {
+				if (multiplechoicequestion.getAttribute ("bg").StartsWith ("@_")) {
+				
+				
+				
+				
+					foreach (QuestRuntimeAsset qra in questactions.photos) {
+					
+						//Debug.Log("KEY:"+qra.key);
+					
+						if (qra.key == multiplechoicequestion.getAttribute ("bg")) {
+						
+						
+						
+						
+							Sprite s = Sprite.Create (qra.texture, new Rect (0, 0, qra.texture.width, qra.texture.height), new Vector2 (0.5f, 0.5f));
+						
+
+							
+							image.sprite = s;
+							image.enabled = true;
+							
+							
+
+						
+						}
+					}
+					//Debug.Log ("donewithforeach");
+				
+				
+				
+				
+				} else {
+				
+				
+				
+				
+					foreach (SpriteConverter sc in questdb.convertedSprites) {
+					
+					
+					
+						if (sc.filename == multiplechoicequestion.getAttribute ("bg")) {
+						
+							if (sc.isDone) {
+								if (sc.sprite != null) {
+						
+									image.sprite = sc.sprite;
+									image.enabled = true;
+									
+									
+
+								
+								
+								
+								
+								
+								} else {
+								
+									Debug.Log ("Sprite was null");
+								}
+							} else {
+							
+								Debug.Log ("SpriteConverter was not done.");
+							
+							}
+						}
+					}
+				
+				}
+			
+			}
+
+
+		}
 	}
 	
 	public void onEnd(){
