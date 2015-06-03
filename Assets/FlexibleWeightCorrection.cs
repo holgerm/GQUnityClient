8f7a7323e67dea4503629ccc2623b8d77a89998e
Holger Muegge <muegge@acm.org>
2015-06-03 20:49:54 +0200

    npc image adapted a bit

--- Assets/FlexibleWeightCorrection.cs
+++ Assets/FlexibleWeightCorrection.cs
@@ -9,33 +9,40 @@ public class FlexibleWeightCorrection : MonoBehaviour
 	public float myWeight = 4f;
 	public float weightOfRest = 4f;
 
-	public float standardScreenAspectRatio = 4f / 3f;
+	public float standardScreenAspectRatio = 1.2f;
 	public LayoutElement layoutElement;
 
 
 	// Use this for initialization
 	void Start ()
 	{
+//		holgersVersion ();
+
+		kevinsVersion ();
+	
+	}
+
+	void kevinsVersion ()
+	{
 		float parentWidth = Camera.main.pixelWidth; 
 		float parentHeighth = Camera.main.pixelHeight;
-
+		
 		float weightSum = myWeight + weightOfRest;
-
+		
 		float newHeight = parentWidth * standardScreenAspectRatio * (myWeight / weightSum);
 		float newWeight = newHeight * (weightOfRest / (parentHeighth - newHeight));
-
+		
 		GetComponent<LayoutElement> ().flexibleHeight = newWeight;
-	
+		
 	}
 
 	void holgersVersion ()
 	{
 		Rect rect = GetComponent<RectTransform> ().rect;
 		
-		Rect parentRect = gameObject.transform.parent.GetComponent<RectTransform> ().rect;
-		
-		Debug.Log ("parentRect.width : parentRect.height  " + parentRect.width + " : " + parentRect.height);
-		
+		float parentWidth = Camera.main.pixelWidth; 
+		float parentHeighth = Camera.main.pixelHeight;
+
 		float imageWidth = 1000f;
 		float imageHeight = 100f; // menu bar hidden
 		
@@ -51,8 +58,8 @@ public class FlexibleWeightCorrection : MonoBehaviour
 		
 		float thisHeightShare = 
 			//			(parentRect.width / parentRect.height) * 
-			(1080f / 1920f) * 
-			(Mathf.Min ((2f / 3f), imageHeight / imageWidth));
+			(parentWidth / parentHeighth) * 
+			(Mathf.Min ((1f / standardScreenAspectRatio), imageHeight / imageWidth));
 		
 		float myWeight = thisHeightShare * weightOfRest / (1 - thisHeightShare);
 		
