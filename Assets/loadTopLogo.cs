aa928b0e897568a5af09b5369683269451a07cdb
Kevin Glaap <kevin.glaap@gmail.com>
2015-06-09 12:01:26 +0200

    LWL Design

---
+++ Assets/loadTopLogo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class loadTopLogo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+		GetComponent<Image> ().sprite = Configuration.instance.toplogo;
+	}
+	
+
+}
