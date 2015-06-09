a4a7344b21021742383fc6491a511c2a5c987034
Holger Muegge <muegge@acm.org>
2015-06-09 10:07:18 +0200

    call regexp matcher just once per loop

--- Assets/Scripts/page_textquestion.cs
+++ Assets/Scripts/page_textquestion.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Text.RegularExpressions;
 
-public class page_textquestion : MonoBehaviour {
+public class page_textquestion : MonoBehaviour
+{
 
 	
 	
@@ -20,16 +21,17 @@ public class page_textquestion : MonoBehaviour {
 	public InputField input;
 	
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		questdb = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ();
 
 		quest = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().currentquest;
 		textquestion = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().currentquest.currentpage;
 		
 		
-		if(textquestion.onStart != null){
+		if (textquestion.onStart != null) {
 			
-			textquestion.onStart.Invoke();
+			textquestion.onStart.Invoke ();
 		}
 		
 		
@@ -43,77 +45,79 @@ public class page_textquestion : MonoBehaviour {
 
 
 
-	public void checkAnswerFinal(){
+	public void checkAnswerFinal ()
+	{
 		string x = input.text;
 
 		bool repeat = false;
 		
-		if(textquestion.hasAttribute("loopUntilSuccess")){
-			if(textquestion.getAttribute("loopUntilSuccess") == "true"){
+		if (textquestion.hasAttribute ("loopUntilSuccess")) {
+			if (textquestion.getAttribute ("loopUntilSuccess") == "true") {
 				repeat = true;
 			} 
 		}
 
 		if (textquestion.contents_answers.Count > 0) {
 
-						bool b = false;
+			bool b = false;
+			bool match;
 
+			foreach (QuestContent y in textquestion.contents_answers) {
+				match = Regex.IsMatch (x, y.content, RegexOptions.IgnoreCase);
 
-			foreach(QuestContent y in textquestion.contents_answers){
+				questdb.debug ("REGEXP " + x + " MATCH " + y.content + " -> " + match);
 
-
-				questdb.debug("REGEXP "+x+" MATCH "+y.content+" -> "+Regex.IsMatch(x, y.content, RegexOptions.IgnoreCase));
-
-				if(y.content == x || Regex.IsMatch(x, y.content, RegexOptions.IgnoreCase)){
+				if (y.content == x || match) {
 					b = true;
 				}
 
 
 			}
 
-			if(b){
+			if (b) {
 				textquestion.state = "succeeded";
 
-				onSuccess();
+				onSuccess ();
 			} else {
 
 
 
-				if(!repeat){
-				textquestion.state = "failed";
+				if (!repeat) {
+					textquestion.state = "failed";
 
-				onFailure();
+					onFailure ();
 				}
 
 			}
 	
-				}
+		}
 		textquestion.result = x;
 
 
 		if (textquestion.state == "succeeded" || !repeat) {
 
-						onEnd ();
-
-				}
-
-
-
+			onEnd ();
 
 		}
 
 
 
-	public void checkAnswerMid(string c){
+
+	}
+
+
+
+	public void checkAnswerMid (string c)
+	{
 		if (textquestion.contents_answers.Count > 0) {
 			
 			string x = input.text;
 			bool b = false;
 			
 			
-			foreach(QuestContent y in textquestion.contents_answers){
+			foreach (QuestContent y in textquestion.contents_answers) {
 
-				if(y.content == x || Regex.IsMatch(x, y.content, RegexOptions.IgnoreCase)){
+				if (y.content == x || Regex.IsMatch (x, y.content, RegexOptions.IgnoreCase)) {
 					b = true;
 
 				}
@@ -122,9 +126,9 @@ public class page_textquestion : MonoBehaviour {
 			}
 			textquestion.result = x;
 
-			if(b){
+			if (b) {
 				textquestion.state = "succeeded";
-				onSuccess();
+				onSuccess ();
 				onEnd ();
 			} 
 			
@@ -139,7 +143,8 @@ public class page_textquestion : MonoBehaviour {
 
 
 	
-	public void onEnd(){
+	public void onEnd ()
+	{
 		
 		if (textquestion.state != "failed") {
 			textquestion.state = "succeeded";
@@ -149,9 +154,9 @@ public class page_textquestion : MonoBehaviour {
 		if (textquestion.onEnd != null) {
 			
 			textquestion.onEnd.Invoke ();
-		} else if(!textquestion.onSuccess.hasMissionAction()  && !textquestion.onFailure.hasMissionAction()) {
+		} else if (!textquestion.onSuccess.hasMissionAction () && !textquestion.onFailure.hasMissionAction ()) {
 			
-			GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().endQuest();
+			GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().endQuest ();
 			
 		}
 		
@@ -159,7 +164,8 @@ public class page_textquestion : MonoBehaviour {
 	}
 	
 	
-	public void onSuccess(){
+	public void onSuccess ()
+	{
 		
 		textquestion.state = "succeeded";
 		
@@ -171,7 +177,8 @@ public class page_textquestion : MonoBehaviour {
 		
 		
 	}
-	public void onFailure(){
+	public void onFailure ()
+	{
 		
 		textquestion.state = "failed";
 		
