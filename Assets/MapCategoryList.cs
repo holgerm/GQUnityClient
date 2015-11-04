2243b438f411dc930ab3cc6902ff65dabda55e15
Holger Muegge <muegge@acm.org>
2015-11-04 18:35:27 +0100

    warning obeyed and refatored a little

--- Assets/MapCategoryList.cs
+++ Assets/MapCategoryList.cs
@@ -1,24 +1,28 @@
 ﻿using UnityEngine;
+
 using System.Collections;
 
-public class MapCategoryList : MonoBehaviour {
+public class MapCategoryList : MonoBehaviour
+{
 
 
 	public GameObject prefab;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 
 
 
 		foreach (MarkerCategorySprite mcs in Configuration.instance.categoryMarker) {
 
-			if(mcs.showInList){
+			if (mcs.showInList) {
 
-			GameObject go = (GameObject)Instantiate(prefab);
-			go.transform.parent = this.transform;
-			go.transform.localScale = new Vector3(1f,1f,1f);
-			go.GetComponent<MapCategoryMenuEntry>().markerCategory = mcs;
+				GameObject go = (GameObject)Instantiate (prefab);
+//				go.transform.parent = this.transform;
+				go.transform.SetParent (this.transform, false);
+				go.transform.localScale = new Vector3 (1f, 1f, 1f);
+				go.GetComponent<MapCategoryMenuEntry> ().markerCategory = mcs;
 
 			}
 
