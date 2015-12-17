800243acf199aef450331626866e97835295ed0c
holgerm <mail@holgermuegge.de>
2015-12-17 10:49:01 +0100

    begun with refactored translation stuff (not used yet) TODO

---
+++ Assets/Scripts/GQ/Lang/SetLanguageComp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Adds functionality to select current translation taarget language by the underlying game objects. E.g. useful for language buttons, menus etc.
+/// </summary>
+public class SetLanguageComp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GQ/Lang/TranslateComp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Adds text translation functionality to game objects. 
+/// 
+/// TODO: Should it also add image translation functionality?
+/// </summary>
+public class TranslateComp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
