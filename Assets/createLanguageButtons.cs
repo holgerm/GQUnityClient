28d49ccad480435b7faaaafe03ab0185ec2e95d2
Kevin Glaap <kevin.glaap@gmail.com>
2015-12-18 15:17:23 +0100

    Language CHoose

---
+++ Assets/createLanguageButtons.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createLanguageButtons : MonoBehaviour
+{
+
+
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+
+
+		foreach (Language l in Configuration.instance.languagesAvailable) {
+
+
+
+			if (l.available) {
+
+
+
+
+				GameObject newButton = Instantiate (prefab) as GameObject;
+				languageButton button = newButton.GetComponent <languageButton> ();
+				button.spr.sprite = l.sprite;
+				button.name.text = l.anzeigename_de;
+				button.bezeichnung = l.bezeichnung;
+				newButton.transform.SetParent (transform);
+				newButton.transform.localScale = new Vector3 (1f, 1f, 1f);
+
+			}
+
+
+
+		}
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Assets/languageButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class languageButton : MonoBehaviour
+{
+
+
+
+	public string bezeichnung;
+
+	public Text name;
+	public Image spr;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+
+
+
+	public void setLanguage ()
+	{
+
+		if (GameObject.Find ("QuestDatabase") != null) {
+
+			GameObject.Find ("QuestDatabase").GetComponent<Dictionary> ().setLanguage (bezeichnung);
+			GameObject.Find ("LanguageAnimator").GetComponent<Animator> ().SetTrigger ("p_out");
+
+		}
+
+
+	}
+	
+
+}
