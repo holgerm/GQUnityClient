84264877e1ade6e7f4310c0a06105055f0e12e4c
holgerm <mail@holgermuegge.de>
2015-12-18 13:59:25 +0100

    commented trial on map touches

--- Assets/Plugins/UnitySlippyMap/Input/Input.cs
+++ Assets/Plugins/UnitySlippyMap/Input/Input.cs
@@ -20,115 +20,120 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-namespace UnitySlippyMap.Input
-{
+namespace UnitySlippyMap.Input {
 
 	/// <summary>
 	/// Input delegate.
 	/// </summary>
-	public delegate void InputDelegate(Map map, bool wasInputInterceptedByGUI);
+	public delegate void InputDelegate (Map map,bool wasInputInterceptedByGUI);
 	
-	public static class MapInput
-	{
+	public static class MapInput {
 		private static Vector3	lastHitPosition = Vector3.zero;
 		private static float	lastZoomFactor = 0.0f;
+		private static bool touchBeganOverGameObject = false;
 		
-		public static void BasicTouchAndKeyboard(Map map, bool wasInputInterceptedByGUI)
-		{
-    		// handle inputs on touch devices and desktop
-    		// the map is told to update its layers and markers once a movement is complete
-    		// when panning the map, the map's root GameObject is moved ; once the panning is done, all the children are offseted and the root's position is reset
-    		bool panning = false;
-    		bool panningStopped = false;
-    		Vector3 screenPosition = Vector3.zero;
+		public static void BasicTouchAndKeyboard (Map map, bool wasInputInterceptedByGUI) {
+			// handle inputs on touch devices and desktop
+			// the map is told to update its layers and markers once a movement is complete
+			// when panning the map, the map's root GameObject is moved ; once the panning is done, all the children are offseted and the root's position is reset
+			bool panning = false;
+			bool panningStopped = false;
+			Vector3 screenPosition = Vector3.zero;
     
-    		bool zooming = false;
-    		bool zoomingStopped = false;
-    		float zoomFactor = 0.0f;
+			bool zooming = false;
+			bool zoomingStopped = false;
+			float zoomFactor = 0.0f;
 
-			if (Application.platform == RuntimePlatform.IPhonePlayer
-	    		|| Application.platform == RuntimePlatform.Android)
-    		{
-				if (wasInputInterceptedByGUI == false)
-				{
-                int touchCount = UnityEngine.Input.touchCount;
-    			if (touchCount > 0)
-    			{
-    				// movements
-    				panning = true;
-    				panningStopped = true;
+//			touchBeganOverGameObject = false;
+//			foreach ( Touch touch in UnityEngine.Input.touches ) {
+//				if ( touch.phase == TouchPhase.Began ) {
+//					if ( EventSystem.current.IsPointerOverGameObject() ) {
+//						touchBeganOverGameObject = true;
+//					}
+//				}
+//			}
+//
+//			if ( touchBeganOverGameObject ) {
+//				return;
+//			}
+//
+			if ( Application.platform == RuntimePlatform.IPhonePlayer
+				|| Application.platform == RuntimePlatform.Android ) {
+				if ( wasInputInterceptedByGUI == false ) {
+					int touchCount = UnityEngine.Input.touchCount;
+					if ( touchCount > 0 ) {
+						// movements
+						panning = true;
+						panningStopped = true;
                     
-                    int validTouchCount = touchCount;
-    				foreach (Touch touch in UnityEngine.Input.touches)
-    				{
-    					if (touch.phase != TouchPhase.Ended)
-                        {
-    	                    screenPosition += new Vector3(touch.position.x, touch.position.y);
-        					panningStopped = false;
-                        }
-                        else
-                        {
-                            --validTouchCount;
-                        }
+						int validTouchCount = touchCount;
+						foreach ( Touch touch in UnityEngine.Input.touches ) {
+							if ( touch.phase != TouchPhase.Ended ) {
+								screenPosition += new Vector3(touch.position.x, touch.position.y);
+								panningStopped = false;
+							}
+							else {
+								--validTouchCount;
+							}
     					
-    					// reset the last hit position to avoid a sudden jump when a finger is added or removed
-    					if (touch.phase == TouchPhase.Began
-                            || touch.phase == TouchPhase.Ended)
-    						lastHitPosition = Vector3.zero;
-    				}
+							// reset the last hit position to avoid a sudden jump when a finger is added or removed
+							if ( touch.phase == TouchPhase.Began
+								|| touch.phase == TouchPhase.Ended ) {
+								lastHitPosition = Vector3.zero;
+							}
+						}
     				
-                    if (validTouchCount != 0)
-                        screenPosition /= validTouchCount;
-                    else
-                    {
-                        screenPosition = Vector3.zero;
-                        panningStopped = true;
-                    }
+						if ( validTouchCount != 0 ) {
+							screenPosition /= validTouchCount;
+						}
+						else {
+							screenPosition = Vector3.zero;
+							panningStopped = true;
+						}
                     
-                    //Debug.Log("DEBUG: panning: touch count: " + touchCount + ", screen pos: (" + screenPosition.x + " " + screenPosition.y + " " + screenPosition.z + "), panning stopped: " + panningStopped);
+						//Debug.Log("DEBUG: panning: touch count: " + touchCount + ", screen pos: (" + screenPosition.x + " " + screenPosition.y + " " + screenPosition.z + "), panning stopped: " + panningStopped);
                     
-    				if (panningStopped)
-    					panning = false;
-                }
+						if ( panningStopped ) {
+							panning = false;
+						}
+					}
                 
-                if (touchCount > 1)
-                {
-    				// zoom
-    				zooming = true;
-    				zoomingStopped = true;
-    				bool newFingerSetup = false;
+					if ( touchCount > 1 ) {
+						// zoom
+						zooming = true;
+						zoomingStopped = true;
+						bool newFingerSetup = false;
 
-                    int validTouchCount = touchCount;
-                    for (int i = 0; i < touchCount; ++i)
-    				{
-                        Touch touch = UnityEngine.Input.GetTouch(i);
+						int validTouchCount = touchCount;
+						for ( int i = 0; i < touchCount; ++i ) {
+							Touch touch = UnityEngine.Input.GetTouch(i);
                         
-    					if (touch.phase != TouchPhase.Ended)
-                        {
-                            zoomFactor += Vector3.Distance(screenPosition, new Vector3(touch.position.x, touch.position.y));
-    						zoomingStopped = false;
-                        }
-                        else
-                        {
-                            --validTouchCount;
-                        }
+							if ( touch.phase != TouchPhase.Ended ) {
+								zoomFactor += Vector3.Distance(screenPosition, new Vector3(touch.position.x, touch.position.y));
+								zoomingStopped = false;
+							}
+							else {
+								--validTouchCount;
+							}
     					
-    					// reset the last zoom factor to avoid a sudden jump when a finger is added or removed
-    					if (touch.phase == TouchPhase.Began
-    						|| touch.phase == TouchPhase.Ended)
-    						newFingerSetup = true;
-    				}
+							// reset the last zoom factor to avoid a sudden jump when a finger is added or removed
+							if ( touch.phase == TouchPhase.Began
+								|| touch.phase == TouchPhase.Ended ) {
+								newFingerSetup = true;
+							}
+						}
                     
-                    if (validTouchCount != 0)
-    				    zoomFactor /= validTouchCount * 10.0f;
-                    else
-                    {
-                        zoomFactor = 0.0f;
-                        zoomingStopped = true;
-                    }
+						if ( validTouchCount != 0 ) {
+							zoomFactor /= validTouchCount * 10.0f;
+						}
+						else {
+							zoomFactor = 0.0f;
+							zoomingStopped = true;
+						}
                     
-                    /*
+						/*
                     Debug.Log("DEBUG: zooming: touch count: " + validTouchCount + ", factor: " + zoomFactor + ", zooming stopped: " + zoomingStopped + ", new finger setup: " + newFingerSetup);
                     string dbg = "DEBUG: touches:\n";
                     for (int i = 0; i < touchCount; ++i)
@@ -139,132 +144,131 @@ namespace UnitySlippyMap.Input
                     Debug.Log(dbg);
                     */
     				
-    				if (newFingerSetup)
-    					lastZoomFactor = zoomFactor;
-    				if (zoomingStopped)
-    					zooming = false;
-    			}
+						if ( newFingerSetup ) {
+							lastZoomFactor = zoomFactor;
+						}
+						if ( zoomingStopped ) {
+							zooming = false;
+						}
+					}
 				}
-    		}
-    		else
-    		{
-				if (wasInputInterceptedByGUI == false)
-				{
-	    			// movements
-	    			if (UnityEngine.Input.GetMouseButton(0))
-                    /*
+			}
+			else {
+				if ( wasInputInterceptedByGUI == false ) {
+					// movements
+					if ( UnityEngine.Input.GetMouseButton(0) ) {
+						/*
 					if ((Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseDrag)
 						&& Event.current.button == 0)
                      */
-	    			{
-                        //Debug.LogError("DEBUG: mouse down");
+						//Debug.LogError("DEBUG: mouse down");
 
 
 
 						map.UpdateCenterWithLocation = false;
-                        panning = true;
-	    				screenPosition = UnityEngine.Input.mousePosition;
+						panning = true;
+						screenPosition = UnityEngine.Input.mousePosition;
 						//screenPosition = new Vector2(Event.current.mousePosition.x, Screen.height - Event.current.mousePosition.y);
-	    			}
-	    			else if (UnityEngine.Input.GetMouseButtonUp(0))
-                        /*
+					}
+					else
+					if ( UnityEngine.Input.GetMouseButtonUp(0) ) {
+						/*
 					else if (Event.current.type == EventType.MouseUp
 						&& Event.current.button == 0)
                          */
-	    			{
-                        //Debug.LogError("DEBUG: mouse up");
-	    				panningStopped = true;
-	    			}
+						//Debug.LogError("DEBUG: mouse up");
+						panningStopped = true;
+					}
 	    			
-	    			// zoom
-	    			if (UnityEngine.Input.GetKey(KeyCode.Z))
-	    			{
-	    				zooming = true;
-	    				zoomFactor = 1.0f;
-	    				lastZoomFactor = 0.0f;
-	    			}
-	    			else if (UnityEngine.Input.GetKeyUp(KeyCode.Z))
-	    			{
-	    				zoomingStopped = true;
-	    			}
+					// zoom
+					if ( UnityEngine.Input.GetKey(KeyCode.Z) ) {
+						zooming = true;
+						zoomFactor = 1.0f;
+						lastZoomFactor = 0.0f;
+					}
+					else
+					if ( UnityEngine.Input.GetKeyUp(KeyCode.Z) ) {
+						zoomingStopped = true;
+					}
 	    		
 				}
-    		}
+			}
 			
-    		if (panning)
-    		{
+			if ( panning ) {
 
 
-				if(!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject ()){
+				if ( !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject() ) {
 
 
-    			// disable the centerWGS84 update with the last location
-    			map.UpdateCenterWithLocation = false;
+					// disable the centerWGS84 update with the last location
+					map.UpdateCenterWithLocation = false;
     			
-    			// apply the movements
-    			Ray ray = map.CurrentCamera.ScreenPointToRay(screenPosition);
+					// apply the movements
+					Ray ray = map.CurrentCamera.ScreenPointToRay(screenPosition);
 
 
 
 			
-    			RaycastHit hitInfo;
-    			if (Physics.Raycast(ray, out hitInfo))
-    			{
-    				//Debug.Log("DEBUG: last hit: " + lastHitPosition + ", hit: " + hitInfo.point);
-    				Vector3 displacement = Vector3.zero;
-    				if (lastHitPosition != Vector3.zero)
-    				{
-    					displacement = hitInfo.point - lastHitPosition;
-    					/*
+					RaycastHit hitInfo;
+					if ( Physics.Raycast(ray, out hitInfo) ) {
+						//Debug.Log("DEBUG: last hit: " + lastHitPosition + ", hit: " + hitInfo.point);
+						Vector3 displacement = Vector3.zero;
+						if ( lastHitPosition != Vector3.zero ) {
+							displacement = hitInfo.point - lastHitPosition;
+							/*
     					Vector3 rootPosition = this.gameObject.transform.position;
     					this.gameObject.transform.position = new Vector3(
     						rootPosition.x + displacement.x,
     						rootPosition.y + displacement.y,
     						rootPosition.z + displacement.z);
     						*/
-    				}
-    				lastHitPosition = new Vector3(hitInfo.point.x, hitInfo.point.y, hitInfo.point.z);
-    				//Debug.Log("DEBUG: last hit: " + lastHitPosition + ", hit: " + hitInfo.point);
+						}
+						lastHitPosition = new Vector3(hitInfo.point.x, hitInfo.point.y, hitInfo.point.z);
+						//Debug.Log("DEBUG: last hit: " + lastHitPosition + ", hit: " + hitInfo.point);
     				
-    				if (displacement != Vector3.zero)
-    				{
-    					// update the centerWGS84 property to the new centerWGS84 wgs84 coordinates of the map
-    					double[] displacementMeters = new double[2] { displacement.x / map.RoundedScaleMultiplier, displacement.z / map.RoundedScaleMultiplier };
-    					double[] centerMeters = new double[2] { map.CenterEPSG900913[0], map.CenterEPSG900913[1] };
-    					centerMeters[0] -= displacementMeters[0];
-    					centerMeters[1] -= displacementMeters[1];
-    					map.CenterEPSG900913 = centerMeters;
+						if ( displacement != Vector3.zero ) {
+							// update the centerWGS84 property to the new centerWGS84 wgs84 coordinates of the map
+							double[] displacementMeters = new double[2] {
+								displacement.x / map.RoundedScaleMultiplier,
+								displacement.z / map.RoundedScaleMultiplier
+							};
+							double[] centerMeters = new double[2] {
+								map.CenterEPSG900913[0],
+								map.CenterEPSG900913[1]
+							};
+							centerMeters[0] -= displacementMeters[0];
+							centerMeters[1] -= displacementMeters[1];
+							map.CenterEPSG900913 = centerMeters;
     					
-    #if DEBUG_LOG
+							#if DEBUG_LOG
     					Debug.Log("DEBUG: Map.Update: new centerWGS84 wgs84: " + centerWGS84[0] + ", " + centerWGS84[1]);
-    #endif
-    				}
+							#endif
+						}
     
-    				map.HasMoved = true;
-    			}
+						map.HasMoved = true;
+					}
 				}
-    		}
-    		else if (panningStopped)
-    		{
+			}
+			else
+			if ( panningStopped ) {
 				//Debug.Log("panning stopped");
-    			// reset the last hit position
-    			lastHitPosition = Vector3.zero;
+				// reset the last hit position
+				lastHitPosition = Vector3.zero;
     			
-    			// trigger a tile update
-    			map.IsDirty = true;
-    		}
+				// trigger a tile update
+				map.IsDirty = true;
+			}
     
-    		// apply the zoom
-    		if (zooming)
-    		{			
-    			//if (lastZoomFactor != 0.0f)// && zoomFactor != 0.0f)
-    				map.Zoom(zoomFactor - lastZoomFactor);
-    			lastZoomFactor = zoomFactor;
-    		}
-    		else if (zoomingStopped)
-    		{
-    			lastZoomFactor = 0.0f;
-    		}
+			// apply the zoom
+			if ( zooming ) {			
+				//if (lastZoomFactor != 0.0f)// && zoomFactor != 0.0f)
+				map.Zoom(zoomFactor - lastZoomFactor);
+				lastZoomFactor = zoomFactor;
+			}
+			else
+			if ( zoomingStopped ) {
+				lastZoomFactor = 0.0f;
+			}
 		}
 	}
 }
