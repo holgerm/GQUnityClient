9cc56680dc3a366cc05b566bcb6cdffe28541eb3
holgerm <mail@holgermuegge.de>
2015-12-22 19:06:37 +0100

    Visible version number now specified in Configuration

---
+++ Assets/ShowVersion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class ShowVersion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Text text = GetComponent<Text>();
+		if ( text != null ) {
+			text.text = String.Format("{0}.{1}", Configuration.instance.appVersion.ToString(), Configuration.instance.subversionNumber);
+		}
+		else {
+			Debug.LogError("Text component missing or not initialized (should be used for showing version number)");
+		}
+	}
+	
+}
