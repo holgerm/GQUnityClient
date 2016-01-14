d8f4e08361cb45a8b55b407d85dce94a3620f5dc
Kevin Glaap <kevin.glaap@gmail.com>
2016-01-14 12:53:48 +0100

    Multiple Choice Question: Falls "loopUntilSuccess"-> onEnd nur nach onSuccess ausführen

--- Assets/Scripts/page_multiplechoicequestion.cs
+++ Assets/Scripts/page_multiplechoicequestion.cs
@@ -7,7 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class page_multiplechoicequestion : MonoBehaviour {
+public class page_multiplechoicequestion : MonoBehaviour
+{
 
 
 
@@ -23,10 +24,11 @@ public class page_multiplechoicequestion : MonoBehaviour {
 
 	public questdatabase questdb;
 	public actions 	questactions;
-		public Image image;
+	public Image image;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
 
 		if (GameObject.Find ("QuestDatabase") == null) {
@@ -50,22 +52,22 @@ public class page_multiplechoicequestion : MonoBehaviour {
 
 
 
-				questiontext.text = questdb.GetComponent<actions> ().formatString (multiplechoicequestion.getAttribute ("question"));
+			questiontext.text = questdb.GetComponent<actions> ().formatString (multiplechoicequestion.getAttribute ("question"));
 
 
 			List<QuestContent> answers = multiplechoicequestion.contents_answers;
 
-			if(multiplechoicequestion.hasAttribute ("shuffle") && multiplechoicequestion.getAttribute ("shuffle") == "true"){
+			if (multiplechoicequestion.hasAttribute ("shuffle") && multiplechoicequestion.getAttribute ("shuffle") == "true") {
 
 
 				int n = answers.Count;
-				System.Random rnd = new System.Random();
+				System.Random rnd = new System.Random ();
 				while (n > 1) {
-					int k = (rnd.Next(0, n) % n);
+					int k = (rnd.Next (0, n) % n);
 					n--;
-					QuestContent value = answers[k];
-					answers[k] = answers[n];
-					answers[n] = value;
+					QuestContent value = answers [k];
+					answers [k] = answers [n];
+					answers [n] = value;
 				}
 				
 				
@@ -77,18 +79,18 @@ public class page_multiplechoicequestion : MonoBehaviour {
 			foreach (QuestContent qc in multiplechoicequestion.contents_answers) {
 
 
-					multiplechoiceanswerbutton btn = (multiplechoiceanswerbutton)Instantiate (answerbuttonprefab, transform.position, Quaternion.identity);
-					btn.transform.SetParent (list.transform);
-					btn.transform.localScale = new Vector3 (1f, 1f, 1f);
-					btn.setText (qc.content);
+				multiplechoiceanswerbutton btn = (multiplechoiceanswerbutton)Instantiate (answerbuttonprefab, transform.position, Quaternion.identity);
+				btn.transform.SetParent (list.transform);
+				btn.transform.localScale = new Vector3 (1f, 1f, 1f);
+				btn.setText (qc.content);
 
-					if (qc.getAttribute ("correct") == "1") {
-						btn.correct = true; 
-					} else {
-						btn.correct = false;
+				if (qc.getAttribute ("correct") == "1") {
+					btn.correct = true; 
+				} else {
+					btn.correct = false;
 
-					}
 				}
+			}
 
 
 
@@ -112,9 +114,9 @@ public class page_multiplechoicequestion : MonoBehaviour {
 					multiplechoicequestion.getAttribute ("bg").StartsWith ("https://")
 					) {
 					
-					www = new WWW(multiplechoicequestion.getAttribute("bg"));
+					www = new WWW (multiplechoicequestion.getAttribute ("bg"));
 					
-					StartCoroutine(waitforImage());
+					StartCoroutine (waitforImage ());
 					
 					
 				} else if (multiplechoicequestion.getAttribute ("bg").StartsWith ("@_")) {
@@ -192,23 +194,46 @@ public class page_multiplechoicequestion : MonoBehaviour {
 		}
 	}
 	
-	public void onEnd(){
-		
-		if (multiplechoicequestion.state != "failed") {
-			multiplechoicequestion.state = "succeeded";
-			
+	public void onEnd ()
+	{
+
+
+		bool doit = true;
+
+
+
+		if (multiplechoicequestion.hasAttribute ("loopUntilSuccess")
+			&& multiplechoicequestion.getAttribute ("loopUntilSuccess") == "true"
+			&& multiplechoicequestion.state.Equals ("failed")) {
+
+
+			doit = false;
+
 		}
-		
-		if (multiplechoicequestion.onEnd != null) {
+
+
+
+		if (doit) {
+
+
+
+			if (multiplechoicequestion.state != "failed") {
+				multiplechoicequestion.state = "succeeded";
 			
-			multiplechoicequestion.onEnd.Invoke ();
-		} else if((multiplechoicequestion.onSuccess == null && multiplechoicequestion.onFailure == null)
+			}
+		
+			if (multiplechoicequestion.onEnd != null) {
+			
+				multiplechoicequestion.onEnd.Invoke ();
+			} else if ((multiplechoicequestion.onSuccess == null && multiplechoicequestion.onFailure == null)
 		          ||
-		          ( multiplechoicequestion.onSuccess != null && !multiplechoicequestion.onSuccess.hasMissionAction()  && multiplechoicequestion.onFailure != null && !multiplechoicequestion.onFailure.hasMissionAction())
+				(multiplechoicequestion.onSuccess != null && !multiplechoicequestion.onSuccess.hasMissionAction () && multiplechoicequestion.onFailure != null && !multiplechoicequestion.onFailure.hasMissionAction ())
 		          ) {
 			
-			GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().endQuest();
+				GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().endQuest ();
 			
+			}
+
 		}
 		
 		
@@ -231,8 +256,8 @@ public class page_multiplechoicequestion : MonoBehaviour {
 			Sprite s = Sprite.Create (www.texture, new Rect (0, 0, www.texture.width, www.texture.height), new Vector2 (0.5f, 0.5f));
 
 				
-				image.sprite = s;
-				image.enabled = true;
+			image.sprite = s;
+			image.enabled = true;
 				
 				
 			
@@ -254,7 +279,8 @@ public class page_multiplechoicequestion : MonoBehaviour {
 
 
 
-	public void onSuccess(){
+	public void onSuccess ()
+	{
 		
 		multiplechoicequestion.state = "succeeded";
 
@@ -266,7 +292,8 @@ public class page_multiplechoicequestion : MonoBehaviour {
 		
 		
 	}
-	public void onFailure(){
+	public void onFailure ()
+	{
 		
 		multiplechoicequestion.state = "failed";
 
