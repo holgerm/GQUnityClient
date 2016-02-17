b99be98826dbfb158af9f503d98d1056844943bc
Kevin Glaap <kevin.glaap@gmail.com>
2016-02-17 13:25:43 +0100

    Menu Overhaul #1

--- Assets/MapCategoryMenuEntry.cs
+++ Assets/MapCategoryMenuEntry.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class MapCategoryMenuEntry : MonoBehaviour {
+public class MapCategoryMenuEntry : MonoBehaviour
+{
 
 
 
@@ -12,8 +13,19 @@ public class MapCategoryMenuEntry : MonoBehaviour {
 	public Toggle toggle;
 
 
+
+	public Image backgroundImage;
+	public Image backgroundImageGradient;
+
+	public Color fadeoutbgcolor;
+	public Color fadeoutmarkercolor;
+
+	public Color fadeoutcolor;
+
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 		markerImage.sprite = markerCategory.sprite;
 		text.text = markerCategory.anzeigename_de;
@@ -27,14 +39,30 @@ public class MapCategoryMenuEntry : MonoBehaviour {
 
 
 
-	public void onChange(bool b){
+	public void onChange ()
+	{
 
 
-		markerCategory.showOnMap = b;
+		markerCategory.showOnMap = !markerCategory.showOnMap;
 
 		if (GameObject.Find ("PageController_Map") != null) {
 			GameObject.Find ("PageController_Map").GetComponent<page_map> ().updateMapMarker ();
 
 		}
+
+
+		if (!markerCategory.showOnMap) {
+			backgroundImageGradient.enabled = true;
+			backgroundImage.color = fadeoutbgcolor;
+			markerImage.color = fadeoutmarkercolor;
+			text.color = fadeoutcolor;
+		} else {
+			backgroundImageGradient.enabled = false;
+
+			backgroundImage.color = Color.white;
+			markerImage.color = Color.white;
+			text.color = Color.black;
+		}
+
 	}
 }
--- Assets/translateTextComponent.cs
+++ Assets/translateTextComponent.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 using System.Collections;
 using Candlelight.UI;
 
-public class translateTextComponent : MonoBehaviour {
+public class translateTextComponent : MonoBehaviour
+{
 
 
 	public bool debug = false;
@@ -23,70 +24,69 @@ public class translateTextComponent : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
-		translate();
-
-	}
-
-
-
-	void translate () {
-
-
-
-		if ( translateOnlyPart == "" ) {
-
-			if ( GetComponent<Text>() != null ) {
-				GetComponent<Text>().text = GameObject.Find("QuestDatabase").GetComponent<Dictionary>().getTranslation(GetComponent<Text>().text);
-			}
-			else
-			if ( GetComponent<HyperText>() != null ) {
-				GetComponent<HyperText>().text = GameObject.Find("QuestDatabase").GetComponent<Dictionary>().getTranslation(GetComponent<HyperText>().text);
-			}
-			textcontent = GetComponent<Text>().text;
-			
-		}
-		else {
-			
-			string translation = translateOnlyPart;
-
-			string translationAfter = GameObject.Find("QuestDatabase").GetComponent<Dictionary>().getTranslation(translation);
-			if ( GetComponent<Text>() != null ) {
-				GetComponent<Text>().text = GetComponent<Text>().text.Replace(translation, translationAfter);
-			}
-			else
-			if ( GetComponent<HyperText>() != null ) {
-				GetComponent<HyperText>().text = GetComponent<HyperText>().text.Replace(translation, translationAfter);
-
-			}
-			textcontent = GetComponent<Text>().text;
-				
-				
-		}
-
-
-		currentLanguage = GameObject.Find("QuestDatabase").GetComponent<Dictionary>().language;
+		translate ();
 
 	}
 
 
 
-	void Update () {
-		if ( GameObject.Find("QuestDatabase") == null ) {
+	void translate ()
+	{
+
+		if (GameObject.Find ("QuestDatabase") != null) {
+
+
+			if (translateOnlyPart == "") {
+
+				if (GetComponent<Text> () != null) {
+					GetComponent<Text> ().text = GameObject.Find ("QuestDatabase").GetComponent<Dictionary> ().getTranslation (GetComponent<Text> ().text);
+				} else if (GetComponent<HyperText> () != null) {
+					GetComponent<HyperText> ().text = GameObject.Find ("QuestDatabase").GetComponent<Dictionary> ().getTranslation (GetComponent<HyperText> ().text);
+				}
+				textcontent = GetComponent<Text> ().text;
+			
+			} else {
+			
+				string translation = translateOnlyPart;
+
+				string translationAfter = GameObject.Find ("QuestDatabase").GetComponent<Dictionary> ().getTranslation (translation);
+				if (GetComponent<Text> () != null) {
+					GetComponent<Text> ().text = GetComponent<Text> ().text.Replace (translation, translationAfter);
+				} else if (GetComponent<HyperText> () != null) {
+					GetComponent<HyperText> ().text = GetComponent<HyperText> ().text.Replace (translation, translationAfter);
+
+				}
+				textcontent = GetComponent<Text> ().text;
+				
+				
+			}
+		}
+
+		currentLanguage = GameObject.Find ("QuestDatabase").GetComponent<Dictionary> ().language;
+
+	}
+
+
+
+	void Update ()
+	{
+		if (GameObject.Find ("QuestDatabase") == null) {
 			return;
 		}
 
 		bool done = false;
-		if ( keepTranslatingAtRuntime && !GetComponent<Text>().text.Equals(textcontent) ) {
-			translate();
+		if (keepTranslatingAtRuntime && !GetComponent<Text> ().text.Equals (textcontent)) {
+			translate ();
 			done = true;
 		}
 
 
-		if ( !done && currentLanguage != GameObject.Find("QuestDatabase").GetComponent<Dictionary>().language ) {
+		if (!done && currentLanguage != GameObject.Find ("QuestDatabase").GetComponent<Dictionary> ().language) {
 
-			translate();
+			translate ();
 
 		}
 
