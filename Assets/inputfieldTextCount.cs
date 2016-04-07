5691a72bc6fe19de4f6af4ea1e3b1fa4fb04d00c
Kevin Glaap <kevin.glaap@gmail.com>
2016-04-07 09:03:59 +0200

    Trigger Hotspots through exhibition code

---
+++ Assets/inputfieldTextCount.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class inputfieldTextCount : MonoBehaviour
+{
+
+
+	public int maxCount = 3;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (GetComponent<InputField> ().text.Length > maxCount) {
+
+			GetComponent<InputField> ().text = GetComponent<InputField> ().text.Substring (0, maxCount);
+
+		}
+	
+	}
+}
