9fe79253979e0533695088f916bb0e5f5e88fa5a
Kevin Glaap <kevin.glaap@gmail.com>
2016-04-07 09:43:46 +0200

    Inputfield Number pad

--- Assets/inputfieldTextCount.cs
+++ Assets/inputfieldTextCount.cs
@@ -20,4 +20,18 @@ public class inputfieldTextCount : MonoBehaviour
 		}
 	
 	}
+
+	public void removeString ()
+	{
+
+
+		GetComponent<InputField> ().text = GetComponent<InputField> ().text.Substring (0, GetComponent<InputField> ().text.Length - 1);
+	}
+
+	public void addString (string i)
+	{
+
+
+		GetComponent<InputField> ().text = GetComponent<InputField> ().text + "" + i;
+	}
 }
