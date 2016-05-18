88a16834157f0475d5271d562071b73f1cea4eae
holgerm <mail@holgermuegge.de>
2016-05-18 11:38:56 +0200

    Youtube PLugin added

--- Assets/YoutubePlayer/Scripts/YoutubeVideo.cs
+++ Assets/YoutubePlayer/Scripts/YoutubeVideo.cs
@@ -1,112 +1,93 @@
-﻿//using UnityEngine;
-//using System.Collections;
-//
-//public class YoutubeVideo : MonoBehaviour {
-//
-//	public static YoutubeVideo Instance;
-//	public bool drawBackground;
-//	public Texture2D backgroundImage;
-//	public string serverGetVideoFile = "http://www.quest-mill.com/clientyoutubehelper/getvideo.php";
-//	public page_videoplay controller;
-//	public MeshRenderer plane;
-//
-//
-//	void Start()
-//	{
-//		Instance = this;
-//	}
-//	
-//	public IEnumerator LoadVideo(string vId)
-//	{
-//
-//
-//		//Dont change this url
-//		//If you change the video will not work
-//
-//		Debug.Log(vId);
-//		if(!vId.Contains("/")){
-//
-//
-//		string url = serverGetVideoFile+"?videoid="+vId+"&type=Download";
-//		Debug.Log(url);
-//		WWWForm form = new WWWForm();
-//		form.AddField("key","youtubeDownloader");
-//		WWW www = new WWW(url,form);
-//		yield return www;
-//		string result = www.text;
-//		Debug.Log("URL:"+result);
-//
-//
-//
-//			if(Application.isMobilePlatform){
-//				controller.playMovie(result);
-//			} else {
-//				WWW loadvideo = new WWW(result);
-//				StartCoroutine(videoloaded(loadvideo));
-//			}
-//
-//
-//		} else {
-//			if(Application.isMobilePlatform){
-//
-//				controller.playMovie(vId);
-//			} else {
-//
-//
-//				WWW loadvideo = new WWW(vId);
-//				StartCoroutine(videoloaded(loadvideo));
-//
-//			}
-//
-//		}
-//
-//
-//
-//
-//			
-//			
-//			
-//			
-//
-//
-//
-//
-//
-//	}
-//
-//
-//	IEnumerator videoloaded(WWW videowww){
-//
-//		#if !UNITY_IPHONE && !UNITY_ANDROID
-//
-//		yield return 0;
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//#else
-//
-//		yield return 0;
-//
-//		#endif
-//
-//
-//	}
-//
-//	void OnGUI()
-//	{
-//		GUI.depth = 1;
-//		if(drawBackground)
-//		{
-//			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundImage);
-//		}
-//	}
-//
-//
-//
-//}
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using YoutubeExtractor;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+public class YoutubeVideo : MonoBehaviour {
+
+    public static YoutubeVideo Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+
+    public bool drawBackground = false;
+    public Texture2D backgroundImage;
+
+    public string RequestVideo(string urlOrId, int quality)
+    {
+        ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
+
+        Uri uriResult;
+        bool result = Uri.TryCreate(urlOrId, UriKind.Absolute, out uriResult)
+            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+        if (!result)
+            urlOrId = "https://youtube.com/watch?v=" + urlOrId;
+
+
+        IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(urlOrId, false);
+        VideoInfo video = null;
+
+        //Search for video with desired format and desired resolution, you can filter if your own methods if you need.
+        var enumerator = videoInfos.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            if (enumerator.Current.VideoType == VideoType.Mp4 && enumerator.Current.Resolution == quality)
+            {
+                video = enumerator.Current;
+                break;
+            }
+        }
+
+        if (video.RequiresDecryption)
+        {
+            DownloadUrlResolver.DecryptDownloadUrl(video);
+        }
+
+        Debug.Log("The mp4 is: " + video.DownloadUrl);
+        return video.DownloadUrl;
+    }
+
+    public bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+    {
+        bool isOk = true;
+        // If there are errors in the certificate chain, look at each error to determine the cause.
+        if (sslPolicyErrors != SslPolicyErrors.None)
+        {
+            for (int i = 0; i < chain.ChainStatus.Length; i++)
+            {
+                if (chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown)
+                {
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                    chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                    bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                    if (!chainIsValid)
+                    {
+                        isOk = false;
+                    }
+                }
+            }
+        }
+        return isOk;
+    }
+
+	void OnGUI()
+	{
+		GUI.depth = 1;
+		if(drawBackground)
+		{
+			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundImage);
+		}
+	}
+
+}
