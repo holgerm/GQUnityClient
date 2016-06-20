7108138e2c38f284bc9739594fc053416a3348f4
holgerm <mail@holgermuegge.de>
2016-06-20 15:26:46 +0200

    unity test tools updated

---
+++ Assets/Editor/Tests/Net/SendQueueTest.cs
@@ -0,0 +1,57 @@
+﻿using UnityEditor;
+using UnityEngine;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class SendQueueTest {
+
+	[Test]
+	public void EditorTest () {
+		//Arrange
+		List<byte> originalBytes = new List<byte>();
+		List<byte[]> segmentedBytes = new List<byte[]>();
+
+		for ( int i = 0; i < 10000; i++ ) {
+			originalBytes.Add((byte)(i % 256));
+		}
+
+		//Act
+		//Try to rename the GameObject
+		segmentedBytes = SendHelper.prepareToSend(originalBytes);
+
+		List<byte> composedAgainBytes = new List<byte>();
+
+		foreach ( byte[] block in segmentedBytes ) {
+			composedAgainBytes.AddRange(block);
+		}
+
+		//Assert
+		//The object has a new name
+		Assert.AreEqual(segmentedBytes.Count, Mathf.CeilToInt((float)originalBytes.Count / (float)SendHelper.BLOCKSIZE));
+		Assert.AreEqual(originalBytes.Count, composedAgainBytes.Count);
+		Assert.AreEqual(originalBytes, composedAgainBytes);
+	}
+
+	[Test]
+	public void AddSegmentedArray () {
+		// Arrange:
+		List<byte> originalBytes = new List<byte>();
+		List<byte[]> segmentedBytes = new List<byte[]>();
+
+		for ( int i = 0; i < 10000; i++ ) {
+			originalBytes.Add((byte)(i % 256));
+		}
+
+		// Act:
+		// create new sendqueue
+		// add all segments
+
+		// Assert:
+		// queue has start
+		// queue has correct middle parts
+		// queue has end
+
+		// check recombination of all parts
+	}
+
+}
