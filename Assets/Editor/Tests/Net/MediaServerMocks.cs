3732b5ec69ff8021c30f29ea17db227dd736dc59
holgerm <mail@holgermuegge.de>
2016-06-20 12:05:05 +0200

    test files not yet working

---
+++ Assets/Editor/Tests/Net/MediaServerMocks.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Basic Server Mock Implementation.
+/// </summary>
+public class MediaServerMock : IMediaServer {
+}
---
+++ Assets/Scripts/GQ/Net/IMediaServer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Interface for Media Server. 
+/// </summary>
+public class IMediaServer {
+
+}
