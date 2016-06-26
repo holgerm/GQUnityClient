b18c7804987278b538fe2e9ac0dbf2bd8d09a559
holgerm <mail@holgermuegge.de>
2016-06-26 23:17:08 +0200

    Test data dir and util class defined

---
+++ Assets/Editor/GQTests/TestsUtil.cs
@@ -0,0 +1,14 @@
+﻿namespace GQTests {
+
+	public class TestsUtil {
+
+		static private string _TEST_DATA_BASE_DIR = "Assets/Editor/GQTestsData/";
+
+		static public string TEST_DATA_BASE_DIR {
+			get {
+				return _TEST_DATA_BASE_DIR;
+			}
+		}
+	}
+
+}
