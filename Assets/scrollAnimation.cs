011314c874f206496fe7529dde9e7233624f4815
Kevin Glaap <kevin.glaap@gmail.com>
2016-08-24 16:25:48 +0200

    Animation

---
+++ Assets/scrollAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollAnimation : MonoBehaviour
+{
+
+	// Use this for initialization
+
+	public RectTransform scrollRect;
+
+	public void setPosYTo1000()
+	{
+		if (scrollRect != null)
+		{
+			scrollRect.anchoredPosition = new Vector2(scrollRect.anchoredPosition.x, -1000f);
+		}
+
+	}
+}
