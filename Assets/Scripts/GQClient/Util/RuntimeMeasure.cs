ddd67e94b592e7258bdc03adcc1ddac23c8d5559
holgerm <mail@holgermuegge.de>
2016-09-27 21:24:27 +0200

    stopwatch that does not conflict with Debug namesapce and is easy to use for experiments

---
+++ Assets/Scripts/GQClient/Util/RuntimeMeasure.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class RuntimeMeasure {
+
+	private string message;
+	private Stopwatch stopwatch;
+
+	public RuntimeMeasure (string message) {
+		this.message = message;
+		stopwatch = new Stopwatch();
+		stopwatch.Start();
+	}
+
+	public void Stop () {
+		this.stopwatch.Stop();
+		UnityEngine.Debug.Log("STOPWATCH:   " + message + " took: " + stopwatch.Elapsed);
+	}
+
+}
