eb26939dfa87cc2b487178b63f56d48a79ca89cf
holgerm <mail@holgermuegge.de>
2017-02-09 08:03:46 +0100

    Accept MP$ even in less or better resolution than expected

--- Assets/YoutubePlayer/Scripts/YoutubeVideo.cs
+++ Assets/YoutubePlayer/Scripts/YoutubeVideo.cs
@@ -10,83 +10,86 @@ using System.Net.Security;
 
 public class YoutubeVideo : MonoBehaviour {
 
-    public static YoutubeVideo Instance;
+	public static YoutubeVideo Instance;
 
-    void Awake()
-    {
-        Instance = this;
-    }
-
-
-    public bool drawBackground = false;
-    public Texture2D backgroundImage;
-
-    public string RequestVideo(string urlOrId, int quality)
-    {
-        ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
-
-        Uri uriResult;
-        bool result = Uri.TryCreate(urlOrId, UriKind.Absolute, out uriResult)
-            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-
-        if (!result)
-            urlOrId = "https://youtube.com/watch?v=" + urlOrId;
+	void Awake () {
+		Instance = this;
+	}
 
 
-        IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(urlOrId, false);
-        VideoInfo video = null;
+	public bool drawBackground = false;
+	public Texture2D backgroundImage;
 
-        //Search for video with desired format and desired resolution, you can filter if your own methods if you need.
-        var enumerator = videoInfos.GetEnumerator();
-        while (enumerator.MoveNext())
-        {
-            if (enumerator.Current.VideoType == VideoType.Mp4 && enumerator.Current.Resolution == quality)
-            {
-                video = enumerator.Current;
-                break;
-            }
-        }
+	public string RequestVideo (string urlOrId, int expectedQuality) {
+		ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
 
-        if (video.RequiresDecryption)
-        {
-            DownloadUrlResolver.DecryptDownloadUrl(video);
-        }
+		Uri uriResult;
+		bool result = Uri.TryCreate(urlOrId, UriKind.Absolute, out uriResult)
+		              && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
 
-        Debug.Log("The mp4 is: " + video.DownloadUrl);
-        return video.DownloadUrl;
-    }
+		if ( !result )
+			urlOrId = "https://youtube.com/watch?v=" + urlOrId;
 
-    public bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-    {
-        bool isOk = true;
-        // If there are errors in the certificate chain, look at each error to determine the cause.
-        if (sslPolicyErrors != SslPolicyErrors.None)
-        {
-            for (int i = 0; i < chain.ChainStatus.Length; i++)
-            {
-                if (chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown)
-                {
-                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
-                    chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
-                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
-                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
-                    bool chainIsValid = chain.Build((X509Certificate2)certificate);
-                    if (!chainIsValid)
-                    {
-                        isOk = false;
-                    }
-                }
-            }
-        }
-        return isOk;
-    }
 
-	void OnGUI()
-	{
+		IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(urlOrId, false);
+		VideoInfo video = null;
+		int bestQualityMissing = expectedQuality;
+
+		//Search for video with desired format and desired resolution, you can filter if your own methods if you need.
+		var enumerator = videoInfos.GetEnumerator();
+		while ( enumerator.MoveNext() ) {
+			Debug.Log("VIDEO INFO: " + enumerator.Current.VideoType.ToString() + " RES: " + enumerator.Current.Resolution.ToString());
+			if ( enumerator.Current.VideoType == VideoType.Mp4 && bestQualityMissing > expectedQuality - enumerator.Current.Resolution ) {
+				// we found a solution better than any before:
+				Debug.Log("VIDEO SOLUTION FOUND.");
+				video = enumerator.Current;
+				bestQualityMissing = expectedQuality - enumerator.Current.Resolution;
+
+				if ( bestQualityMissing <= 0 )
+					// we found an optimal solution:
+					break;
+			}
+		}
+
+		if ( video == null ) {
+			Debug.Log("Video URL not valid: " + urlOrId);
+			return null;
+		}
+		else
+			Debug.Log("VIDEO VALID going to play it ...");
+
+		if ( video.RequiresDecryption ) {
+			DownloadUrlResolver.DecryptDownloadUrl(video);
+		}
+
+		Debug.Log("The mp4 is: " + video.DownloadUrl);
+		return video.DownloadUrl;
+	}
+
+	public bool MyRemoteCertificateValidationCallback (System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+		bool isOk = true;
+		// If there are errors in the certificate chain, look at each error to determine the cause.
+		if ( sslPolicyErrors != SslPolicyErrors.None ) {
+			for ( int i = 0; i < chain.ChainStatus.Length; i++ ) {
+				if ( chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown ) {
+					chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+					chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+					chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+					chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+					bool chainIsValid = chain.Build((X509Certificate2)certificate);
+					if ( !chainIsValid ) {
+						isOk = false;
+					}
+				}
+			}
+		}
+		return isOk;
+	}
+
+	void OnGUI () {
 		GUI.depth = 1;
-		if(drawBackground)
-		{
-			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundImage);
+		if ( drawBackground ) {
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundImage);
 		}
 	}
 
