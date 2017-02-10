fc5eeb11886dee5669d730a51a9db4e2a6c75f63
holgerm <mail@holgermuegge.de>
2017-02-10 10:33:38 +0100

    VideoPlay now works for YouTube movies

--- Assets/Scripts/GQClient/UI/Pages/page_videoplay.cs
+++ Assets/Scripts/GQClient/UI/Pages/page_videoplay.cs
@@ -6,219 +6,187 @@ using System.Collections.Generic;
 public class page_videoplay : MonoBehaviour {
 	
 	
-	private WWW www;
 	public questdatabase questdb;
 	public Quest quest;
-	public QuestPage npctalk;
-	//	public YoutubeVideo youtube;
+	public QuestPage page;
+
+	public Texture2D bgImage;
+
+	public const int RESOLUTION = 720;
+
 	private bool videoplayed = false;
 
 	static string filepath;
 
 	public const string YOUTUBE_URL_PREFIX = "https://www.youtube.com/watch?v=";
 
+	private enum VideoKind {
+		Local,
+		HTTP,
+		YouTube
+	}
 
+	private VideoKind URL2VideoKind (string url) {
+		
+		if ( url.StartsWith(YOUTUBE_URL_PREFIX) ) {
+			return VideoKind.YouTube;
+		}		
+		if ( url.StartsWith("http:") || url.StartsWith("https:") ) {
+			return VideoKind.HTTP;
+		}
+		else {
+			return VideoKind.Local;
+		}
+	}
 
-	IEnumerator Start () {
+	public IEnumerator Start () {
+
+		// try to let image and canvas be shown quickly
+		yield return new WaitForEndOfFrame();
 
 		questdb = GameObject.Find("QuestDatabase").GetComponent<questdatabase>();
 		quest = GameObject.Find("QuestDatabase").GetComponent<questdatabase>().currentquest;
-		npctalk = GameObject.Find("QuestDatabase").GetComponent<questdatabase>().currentquest.currentpage;
+		page = GameObject.Find("QuestDatabase").GetComponent<questdatabase>().currentquest.currentpage;
 
-		if ( npctalk.onStart != null ) {
-			
-			npctalk.onStart.Invoke();
+		if ( page.onStart != null ) {
+			page.onStart.Invoke();
 		}
 		
-		
-
-		#if !UNITY_WEBPLAYER && !UNITY_STANDALONE_OSX
-
-		Screen.orientation = ScreenOrientation.LandscapeLeft;
-
-		
-		
-
-		string url = npctalk.getAttribute("file");
-		Debug.Log("We want to play video url = " + url);
-
-		if ( !url.StartsWith("http:") && !url.StartsWith("https:") ) {
-
-			Debug.Log("Starting video url = " + url);
-
-			if ( Application.platform == RuntimePlatform.Android && questdb.currentquest.predeployed ) {
-
-				if ( url.StartsWith(YOUTUBE_URL_PREFIX) ) {
-					PlayYoutubeVideo(url);
-				}
-				else {
-					url = url.Replace(questdb.PATH_2_PREDEPLOYED_QUESTS, "predeployed/quests");
-					Debug.Log("We will play an ordinary Video link: " + url);
-					Handheld.PlayFullScreenMovie(url);
-				}
-			}
-			else {
-				if ( url.StartsWith(YOUTUBE_URL_PREFIX) ) {
-					PlayYoutubeVideo(url);
-
-				}
-				else {
-					Debug.Log("We will play an ordinary Video link: " + url);
-					Handheld.PlayFullScreenMovie("file://" + url);
-				}
-			}
-
-			yield return new WaitForEndOfFrame();
-			yield return new WaitForEndOfFrame();
-			videoplayed = true;
-
-		}
-		else {
-			if ( url.StartsWith(YOUTUBE_URL_PREFIX) ) {
-				PlayYoutubeVideo(url);
-			}
-
-
-			// YOUTUBE OR URL
-
-
-			/*
-
-
-			
-			StartCoroutine (youtube.LoadVideo (url));
-			
-
-
-			*/
-
-
-		}
-
-
-		/*
-
-		if (npctalk.hasAttribute ("url")) {
-
-
-			Debug.Log("has url: "+npctalk.getAttribute ("url"));
-
-
-				
-				string[] splitted = npctalk.getAttribute ("url").ToString ().Split ('=');
-				
-				url = splitted [splitted.Length - 1];
-				
-				
-				StartCoroutine(youtube.LoadVideo(url));
-
-			
-				}
-*/
-
-	
-				
-
-
-
-		
-#else
-
-		yield return null;
-
+#if UNITY_WEBPLAYER 
 		questdb.debug("Video Playback can't be simulated in web-preview right now");
+		OnEnd();
 
-		StartCoroutine(onEnd());
+#elif UNITY_STANDALONE_OSX
+		OnEnd();
 
-
-#endif
-
-
-
-
-	}
-
-	private IEnumerator PlayStreamingVideo (string url) {
-		//        Handheld.PlayFullScreenMovie(url, Color.black, FullScreenMovieControlMode.Full);
-		yield return new WaitForSeconds(1.0f);
-
-		#if !UNITY_WEBPLAYER && !UNITY_STANDALONE_OSX
-
-		Handheld.PlayFullScreenMovie(url);
+#else
+		PlayVideo();
 
 #endif
-
-		yield return new WaitForEndOfFrame();
-		yield return new WaitForEndOfFrame();
-		StartCoroutine(onEnd());
 	}
 
-	private void PlayYoutubeVideo (string url) {
-		// YouTube Videos
+
+	private void PlayVideo () {
+
+		string url = page.getAttribute("file");
+
+		switch ( URL2VideoKind(url) ) {
+			case VideoKind.YouTube:
+				StartCoroutine(PlayYoutubeVideo(url));
+				break;
+			case VideoKind.HTTP:
+				StartCoroutine(PlayHTTPVideo(url));
+				break;
+			case VideoKind.Local:
+				StartCoroutine(PlayLocalVideo(url));
+				break;
+			default:
+				Debug.LogError("Can NOT play this unknown Video kind. Url: " + url);
+				OnEnd();
+				break;
+		}
+	}
+
+
+	IEnumerator PlayYoutubeVideo (string url) {
+		yield return new WaitForEndOfFrame();
+
+		// Extract youtube id from url:
 		string id = url.Substring(YOUTUBE_URL_PREFIX.Length);
 		// cut off rest of url if there is more than the vid id, e.g. language etc.:
 		int endIndex = id.IndexOf('&');
 		if ( endIndex > 0 ) {
 			id = id.Substring(0, endIndex);
 		}
-		Debug.Log("We will play a YouTube Video id = " + id);
-		Handheld.PlayFullScreenMovie(YoutubeVideo.Instance.RequestVideo(id, 720));
 
-		// TODO catch VideoNotAvailableException
-	}
+		// TODO Test for internet connection or wrap the next call by a timeouted thread:
+		Debug.Log("BEFORE");
+		string link = YoutubeVideo.Instance.RequestVideo(id, RESOLUTION);
+		Debug.Log("AFTER");
 
-
-
-	void Update () {
-
-
-		if ( videoplayed ) {
-
-
-			StartCoroutine(onEnd());
-
+		if ( bgImage != null ) {
+			YoutubeVideo.Instance.backgroundImage = bgImage;
+			YoutubeVideo.Instance.drawBackground = true;
+		}
+		else {
+			YoutubeVideo.Instance.drawBackground = false;
 		}
 
-	}
-		
-	//	public void playMovie (string x)
-	//	{
-	//
-	//		StartCoroutine (playMovieFullscreen (x));
-	//		//onEnd();
-	//		videoplayed = true;
-	//
-	//	}
-	//
-	//	public IEnumerator playMovieFullscreen (string x)
-	//	{
-	//
-	//#if !UNITY_WEBPLAYER
-	//		Handheld.PlayFullScreenMovie (x, Color.black, FullScreenMovieControlMode.Full);
-	//#endif
-	//		yield return 0;
-	//	}
+		if ( link == null ) {
+			Debug.LogError("COULD NOT PLAY Video: " + id);
+			OnEnd();
+			yield break;
+		}
+		else {
+			Screen.orientation = ScreenOrientation.LandscapeLeft;
+			yield return new WaitForEndOfFrame();
+			Handheld.PlayFullScreenMovie(link, Color.black, FullScreenMovieControlMode.Full);
+			yield return new WaitForEndOfFrame();
+			yield return new WaitForEndOfFrame();
+		}
 
-	public IEnumerator onEnd () {
+		yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame();
+		Screen.orientation = ScreenOrientation.Portrait;
+		yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame();
+
+		OnEnd();
+	}
+
+	IEnumerator PlayHTTPVideo (string url) {
+		Debug.Log("We will play an HTTP Video link: " + url);
+
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+		Handheld.PlayFullScreenMovie(url);
+		yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame();
+
+		Debug.Log("Finished the HTTP Video from url: " + url);
+
 		yield return new WaitForSeconds(0.1f);
 		Screen.orientation = ScreenOrientation.Portrait;
 		yield return new WaitForSeconds(0.1f);
 
+		OnEnd();
+	}
 
-		npctalk.state = "succeeded";
+
+	IEnumerator PlayLocalVideo (string url) {
+		Debug.Log("We will play a LOCAL Video from file: " + url);
+
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+		Handheld.PlayFullScreenMovie("file://" + url);
+		yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame();
+
+		Debug.Log("Finished the LOCAL Video from url: " + url);
+
+		yield return new WaitForSeconds(0.1f);
+		Screen.orientation = ScreenOrientation.Portrait;
+		yield return new WaitForSeconds(0.1f);
+
+		OnEnd();
+	}
+
+
+	public void OnEnd () {
+		Debug.Log("VIDEO: ON_END()");
+		Screen.orientation = ScreenOrientation.Portrait;
+		page.state = "succeeded";
 		//questdb.AllowAutoRotation (false);
 
-		
-		if ( npctalk.onEnd != null ) {
+
+		if ( page.onEnd != null ) {
 			Debug.Log("onEnd");
-			npctalk.onEnd.Invoke();
+			page.onEnd.Invoke();
 		}
 		else {
-			
-			GameObject.Find("QuestDatabase").GetComponent<questdatabase>().endQuest();
-			
-		}
 
-		
+			GameObject.Find("QuestDatabase").GetComponent<questdatabase>().endQuest();
+
+		}
 	}
 }
