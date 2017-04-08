647a63a1b7ee03a7566768bb1f88ae334ed74e7b
holgerm <mail@holgermuegge.de>
2017-04-08 14:31:38 +0200

    Starts main scene in editor if active at startup

--- Assets/Code/GQClient/UI/Pages/page_audiorecord.cs
+++ Assets/Code/GQClient/UI/Pages/page_audiorecord.cs
@@ -3,8 +3,10 @@ using UnityEngine.UI;
 
 using System.Collections;
 using GQ.Client.Model;
+using UnityEngine.SceneManagement;
 
-public class page_audiorecord : MonoBehaviour {
+public class page_audiorecord : MonoBehaviour
+{
 	//A boolean that flags whether there's a connected microphone
 	public bool micConnected = false;
 	
@@ -45,25 +47,31 @@ public class page_audiorecord : MonoBehaviour {
 	private float playlength = 0f;
 	
 	// Use this for initialization
-	IEnumerator Start () {
+	IEnumerator Start ()
+	{
 		
 		
+		if (GameObject.Find ("QuestDatabase") == null) {
+
+			SceneManager.LoadScene ("questlist", LoadSceneMode.Single);
+			yield break;
+		} 
+
+
+		questdb = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ();
+		actioncontroller = GameObject.Find ("QuestDatabase").GetComponent<actions> ();
+		quest = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().currentquest;
+		audiorecord = GameObject.Find ("QuestDatabase").GetComponent<questdatabase> ().currentquest.currentpage;
 		
-		questdb = GameObject.Find("QuestDatabase").GetComponent<questdatabase>();
-		actioncontroller = GameObject.Find("QuestDatabase").GetComponent<actions>();
-		quest = GameObject.Find("QuestDatabase").GetComponent<questdatabase>().currentquest;
-		audiorecord = GameObject.Find("QuestDatabase").GetComponent<questdatabase>().currentquest.currentpage;
 		
-		
-		if ( audiorecord.onStart != null ) {
+		if (audiorecord.onStart != null) {
 			
-			audiorecord.onStart.Invoke();
+			audiorecord.onStart.Invoke ();
 		}
 		
-		if ( audiorecord.hasAttribute("task") && audiorecord.getAttribute("task").Length > 1 ) {
-			text.text = questdb.GetComponent<actions>().formatString(audiorecord.getAttribute("task"));
-		}
-		else {
+		if (audiorecord.hasAttribute ("task") && audiorecord.getAttribute ("task").Length > 1) {
+			text.text = questdb.GetComponent<actions> ().formatString (audiorecord.getAttribute ("task"));
+		} else {
 			
 			text.enabled = false;
 			textbg.enabled = false;
@@ -73,49 +81,49 @@ public class page_audiorecord : MonoBehaviour {
 	
 		
 		// init microphone;
-		if ( Application.isWebPlayer ) {
-			yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+		if (Application.isWebPlayer) {
+			yield return Application.RequestUserAuthorization (UserAuthorization.Microphone);
 		}
 		
 	
 		
 	
 		//Check if there is at least one microphone connected  
-		if ( Microphone.devices.Length <= 0 ) {  
+		if (Microphone.devices.Length <= 0) {  
 			//Throw a warning message at the console if there isn't  
-			questdb.debug("Kein Mikrofon verbunden");
-			onEnd();
-		}
-		else { //At least one microphone is present  
+			questdb.debug ("Kein Mikrofon verbunden");
+			onEnd ();
+		} else { //At least one microphone is present  
 			//Set 'micConnected' to true  
 			micConnected = true;  
 			
 			//Get the default microphone recording capabilities  
-			Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);  
+			Microphone.GetDeviceCaps (null, out minFreq, out maxFreq);  
 			
 			//According to the documentation, if minFreq and maxFreq are zero, the microphone supports any frequency...  
-			if ( minFreq == 0 && maxFreq == 0 ) {  
+			if (minFreq == 0 && maxFreq == 0) {  
 				//...meaning 44100 Hz can be used as the recording sampling rate  
 				maxFreq = 44100;  
 			}  
 			
 			//Get the attached AudioSource component  
-			goAudioSource = this.GetComponent<AudioSource>();  
+			goAudioSource = this.GetComponent<AudioSource> ();  
 		}  
 	}
 
 
-	void Update () {
-		if ( Microphone.IsRecording(null) ) { 
+	void Update ()
+	{
+		if (Microphone.IsRecording (null)) { 
 
 			length += Time.deltaTime;
 
 		}
-		if ( playing ) {
+		if (playing) {
 			playlength += Time.deltaTime;
 
-			if ( !goAudioSource.isPlaying ) {
-				playbutton.GetComponentInChildren<Text>().text = "Abspielen";
+			if (!goAudioSource.isPlaying) {
+				playbutton.GetComponentInChildren<Text> ().text = "Abspielen";
 				
 				playing = false;
 
@@ -128,21 +136,21 @@ public class page_audiorecord : MonoBehaviour {
 
 
 
-	public void play () {
+	public void play ()
+	{
 
 
 
-		if ( goAudioSource.isPlaying ) {
+		if (goAudioSource.isPlaying) {
 			playlength = 0f;
-			goAudioSource.Stop();
-			playbutton.GetComponentInChildren<Text>().text = "Abspielen";
+			goAudioSource.Stop ();
+			playbutton.GetComponentInChildren<Text> ().text = "Abspielen";
 
 			playing = false;
-		}
-		else {
-			playbutton.GetComponentInChildren<Text>().text = "Stoppen";
+		} else {
+			playbutton.GetComponentInChildren<Text> ().text = "Stoppen";
 
-			goAudioSource.Play();
+			goAudioSource.Play ();
 			playing = true;
 
 		}
@@ -150,54 +158,54 @@ public class page_audiorecord : MonoBehaviour {
 
 	}
 
-	public void retry () {
-		if ( !Microphone.IsRecording(null) ) { 
+	public void retry ()
+	{
+		if (!Microphone.IsRecording (null)) { 
 			savedaudio = false;
-			recordbutton.GetComponentInChildren<Text>().text = "Aufnahme stoppen";
+			recordbutton.GetComponentInChildren<Text> ().text = "Aufnahme stoppen";
 			playbutton.enabled = false;
-			playbutton.GetComponentInChildren<Text>().enabled = false;
-			playbutton.GetComponent<Image>().enabled = false;
+			playbutton.GetComponentInChildren<Text> ().enabled = false;
+			playbutton.GetComponent<Image> ().enabled = false;
 
 			length = 0f;
 
 			retrybutton.enabled = false;
-			retrybutton.GetComponentInChildren<Text>().enabled = false;
-			retrybutton.GetComponent<Image>().enabled = false;
+			retrybutton.GetComponentInChildren<Text> ().enabled = false;
+			retrybutton.GetComponent<Image> ().enabled = false;
 
-			goAudioSource.clip = Microphone.Start(null, true, 60, maxFreq);  
+			goAudioSource.clip = Microphone.Start (null, true, 60, maxFreq);  
 		}
 
 	}
 
-	public void record () {
+	public void record ()
+	{
 
-		if ( micConnected ) {  
+		if (micConnected) {  
 
 			//If the audio from any microphone isn't being captured  
-			if ( !Microphone.IsRecording(null) && !savedaudio ) {  
+			if (!Microphone.IsRecording (null) && !savedaudio) {  
 
-				goAudioSource.clip = Microphone.Start(null, true, 60, maxFreq);  
-				recordbutton.GetComponentInChildren<Text>().text = "Aufnahme stoppen";
-
-
-
-			}
-			else
-			if ( !savedaudio ) {
+				goAudioSource.clip = Microphone.Start (null, true, 60, maxFreq);  
+				recordbutton.GetComponentInChildren<Text> ().text = "Aufnahme stoppen";
 
 
-				Microphone.End(null); //Stop the audio recording  
+
+			} else if (!savedaudio) {
+
+
+				Microphone.End (null); //Stop the audio recording  
 
 				playbutton.enabled = true;
-				playbutton.GetComponent<Image>().enabled = true;
-				playbutton.GetComponentInChildren<Text>().enabled = true;
+				playbutton.GetComponent<Image> ().enabled = true;
+				playbutton.GetComponentInChildren<Text> ().enabled = true;
 
 				retrybutton.enabled = true;
-				retrybutton.GetComponent<Image>().enabled = true;
+				retrybutton.GetComponent<Image> ().enabled = true;
 
-				retrybutton.GetComponentInChildren<Text>().enabled = true;
+				retrybutton.GetComponentInChildren<Text> ().enabled = true;
 
-				recordbutton.GetComponentInChildren<Text>().text = "Abspeichern";
+				recordbutton.GetComponentInChildren<Text> ().text = "Abspeichern";
 				savedaudio = true;
 
 				AudioClip ac = goAudioSource.clip;
@@ -208,37 +216,35 @@ public class page_audiorecord : MonoBehaviour {
 				float samplesL = (float)ac.samples;
 				float samplesPerSec = samplesL / lengthL;
 				float[] samples = new float[(int)(samplesPerSec * length)];
-				ac.GetData(samples, 0);
-				goAudioSource.clip = AudioClip.Create("RecordedSound", (int)(length * samplesPerSec), 1, (int)samplesPerSec, false, false);
+				ac.GetData (samples, 0);
+				goAudioSource.clip = AudioClip.Create ("RecordedSound", (int)(length * samplesPerSec), 1, (int)samplesPerSec, false, false);
 				//Debug.Log("samples new:"+(length*samplesPerSec));
-				goAudioSource.clip.SetData(samples, 0);
+				goAudioSource.clip.SetData (samples, 0);
 				//Debug.Log ((goAudioSource.clip.samples)/(goAudioSource.clip.length));
 
 
 
-			}
-			else {
+			} else {
 
 				// save to var
 
-				if ( audiorecord.hasAttribute("file") ) {
+				if (audiorecord.hasAttribute ("file")) {
 					
-					QuestRuntimeAsset qra = new QuestRuntimeAsset("@_" + audiorecord.getAttribute("file"), goAudioSource.clip);
+					QuestRuntimeAsset qra = new QuestRuntimeAsset ("@_" + audiorecord.getAttribute ("file"), goAudioSource.clip);
 					
-					actioncontroller.audioclips.Add(qra);
+					actioncontroller.audioclips.Add (qra);
 
 				}
-				onEnd();
+				onEnd ();
 
 
 			}
 
 
 
-		}
-		else {
+		} else {
 
-			onEnd();
+			onEnd ();
 
 		}
 
@@ -247,19 +253,19 @@ public class page_audiorecord : MonoBehaviour {
 
 
 
-	void onEnd () {
+	void onEnd ()
+	{
 		
 		audiorecord.state = "succeeded";
 		
 		
 		
-		if ( audiorecord.onEnd != null && audiorecord.onEnd.actions.Count > 0 ) {
-			Debug.Log("onEnd");
-			audiorecord.onEnd.Invoke();
-		}
-		else {
+		if (audiorecord.onEnd != null && audiorecord.onEnd.actions.Count > 0) {
+			Debug.Log ("onEnd");
+			audiorecord.onEnd.Invoke ();
+		} else {
 			
-			questdb.endQuest();
+			questdb.endQuest ();
 			
 		}
 		
