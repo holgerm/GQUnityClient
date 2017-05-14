3a0b8942766457d5cf80898547c29b87e2a8d2e8
holgerm <mail@holgermuegge.de>
2017-05-14 12:54:37 +0200

    prod wcc restored

---
+++ Production/products/wcc/Editor/LoadingCanvas.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.Animations;
+
+namespace GQ.Client.Conf {
+
+	public class LoadingCanvas {
+
+		public static void Init (GameObject loadingCanvas) {
+
+			UnityEngine.Animator anim = loadingCanvas.GetComponent<UnityEngine.Animator>();
+
+			AnimatorController loadedCtrl = Resources.Load<AnimatorController>("loadingCanvas/LoadingController");
+
+			// ensure that state contains right animation motion:
+			Motion loadedMotion = Resources.Load<Motion>("loadingCanvas/LoadingAnimation");
+			bool hasLink = hasMotionLink(loadedCtrl, loadedMotion);
+
+			if ( !hasMotionLink(loadedCtrl, loadedMotion) )
+				loadedCtrl.AddMotion(loadedMotion);
+
+			// ensure that animator links to controller
+			if ( anim.runtimeAnimatorController == null ) {
+				anim.runtimeAnimatorController = loadedCtrl;
+			}
+
+		}
+
+		private static bool hasMotionLink (AnimatorController ctrl, Motion searchedMotion) {
+			AnimatorControllerLayer[] layers = ctrl.layers;
+			if ( layers.Length == 0 )
+				return false;
+
+			ChildAnimatorState[] states = layers[0].stateMachine.states;
+
+			foreach ( var stateStruct in states ) {
+				if ( stateStruct.state.motion != null ) {
+					if ( stateStruct.state.motion.Equals(searchedMotion) )
+						return true; 
+				}
+				else {
+					stateStruct.state.motion = searchedMotion;
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
