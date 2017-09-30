efbfd417ddd9c8db4fba923f6339e41a8444d213
Qeevee Developer <qeeveedeveloper@Qeevees-MacBook-Air.local>
2017-09-30 16:25:10 +0200

    Serialization of QuestInfos reworked, so that we can have separate setters triggering events

--- Assets/Code/GQClient/Model/mgmt/questinfos/QuestInfo.cs
+++ Assets/Code/GQClient/Model/mgmt/questinfos/QuestInfo.cs
@@ -13,7 +13,7 @@ namespace GQ.Client.Model {
 	/// 
 	/// A questInfo object has the following live cycle / states:
 	/// 
-	/// - The Quest exists only on Server and has not been downloaded yet or has just been delted. 
+	/// - The Quest exists only on Server and has not been downloaded yet or has just been deleted. 
 	///   (Initially if not predeployed)
 	/// 	- Can be downloaded
 	/// 	- Can NOT be started
@@ -63,50 +63,116 @@ namespace GQ.Client.Model {
 
 
 	/// </summary>
+	[JsonObject(MemberSerialization.OptIn)]
 	public class QuestInfo : IComparable<QuestInfo>
 	{
 		#region Serialized Features
+		[JsonProperty]
+		private 	int 	id;
+		public 		int  	Id		{ 
+			get {
+				return id;
+			} 
+		}
 
-		public int  			Id     				{ get; set; }
+		[JsonProperty]
+		private 	string	name;
+		public 		string	Name	{ 
+			get {
+				return name;
+			} 
+		}
 
-		public string   		Name  				{ get; set; }
+		[JsonProperty]
+		private 	string	featuredImagePath;
+		public 		string 	FeaturedImagePath	{ 
+			get {
+				return featuredImagePath;
+			} 
+		}
 
-		public string 			FeaturedImagePath	{ get; set; }
+		[JsonProperty]
+		private 	int? 	typeID;
+		public 		int?	TypeID 				{ 
+			get {
+				return typeID;
+			}
+		}
 
-		public int? 			TypeID 				{ get; set; }
-
-		public string 			IconPath			{ get; set; }
+		[JsonProperty]
+		private 	string	iconPath;
+		public 		string 	IconPath			{ 
+			get {
+				return iconPath;
+			} 
+		}
 
 		/// <summary>
 		/// Server-side update timestamp.
 		/// </summary>
 		/// <value>The last update.</value>
-		public long? 			LastUpdate 			{ get; set; }
+		[JsonProperty]
+		private 	long? 	lastUpdate;
+		public 		long? 	LastUpdateOnServer 	{
+			get {
+				return lastUpdate;
+			}
+		}
 
-		public HotspotInfo[] 	Hotspots			{ get; set; }
+		[JsonProperty]
+		private 	HotspotInfo[]	hotspots;
+		public 		HotspotInfo[] 	Hotspots	{ 
+			get {
+				return hotspots;
+			}
+		}
 
-		public MetaDataInfo[] 	Metadata			{ get; set; }
+		[JsonProperty]
+		private 	MetaDataInfo[] 	metadata;
+		public 		MetaDataInfo[] 	Metadata	{ 
+			get {
+				return metadata;
+			}
+		}
 
-		/// <summary>
-		/// Client-side update timestamp;
-		/// </summary>
-		public long? LastUpdateOnDevice = null;
+		[JsonProperty]
+		private 	long?	_lastUpdateOnDevice = null;		
+		public 		long? 	LastUpdateOnDevice {
+			get {
+				return _lastUpdateOnDevice;
+			}
+			set {
+				_lastUpdateOnDevice = value;
+			}
+		}
 
-		public long? TimestampOfPredeployedVersion = null;
+		[JsonProperty]
+		private 	long?	_timestampOfPredeployedVersion = null;
+		public 		long? 	TimestampOfPredeployedVersion {
+			get {
+				return _timestampOfPredeployedVersion;
+			}
+			set {
+				// TODO how will we set this value?
+				_timestampOfPredeployedVersion = value;
+			}
+		}
 
-		public int PlayedTimes = 0;
+		[JsonProperty]
+		private 	int 	_playedTimes = 0;
+		public 		int 	PlayedTimes {
+			get {
+				return _playedTimes;
+			}
+			set {
+				_playedTimes = value;
+			}
+		}
 
 		#endregion
 
 
 		#region Derived features
-
-		[JsonIgnore]
-		public long? LastUpdateOnServer {
-			get {
-				return LastUpdate;
-			}
-		}
 
 		/// <summary>
 		/// Determines whether this quest is locally available. This feature will be used in the UI in future versions.
@@ -244,7 +310,7 @@ namespace GQ.Client.Model {
 			StringBuilder sb = new StringBuilder();
 
 			sb.AppendFormat("{0} (id: {1})\n", Name, Id);
-			sb.AppendFormat("\t last update: {0}", LastUpdate);
+			sb.AppendFormat("\t last server update: {0}", LastUpdateOnServer);
 			sb.AppendFormat("\t type id: {0}", TypeID);
 			sb.AppendFormat("\t icon path: {0}", IconPath);
 			sb.AppendFormat("\t featured image path: {0}", FeaturedImagePath);
