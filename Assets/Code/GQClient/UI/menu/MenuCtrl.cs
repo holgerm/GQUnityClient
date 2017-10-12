2c52268485f0c469df14e1ca4149250588b8f6e7
holgerm <mail@holgermuegge.de>
2017-10-12 15:00:41 +0200

    View Toggle for Map / List of quests begun

---
+++ Assets/Code/GQClient/UI/menu/MenuCtrl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GQ.Client.Err;
+
+public class MenuCtrl : MonoBehaviour
+{
+
+	public void OnChangeQuestInfosViewer (GameObject viewer)
+	{
+		Debug.Log (("We need to change the Quest Infos Viewer to: " + viewer.name).Red ());
+	}
+
+}
