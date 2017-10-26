35a0ccbf64642decbee2fde7e109619a05e99a23
Qeevee Developer <qeeveedeveloper@qeevees-air.speedport_w_724v_typ_a_05011603_05_020>
2017-10-26 17:51:26 +0200

    layout fix in menu size and menu background image

--- Assets/Code/GQClient/UI/layout/HeaderButtonLayoutConfig.cs
+++ Assets/Code/GQClient/UI/layout/HeaderButtonLayoutConfig.cs
@@ -5,35 +5,39 @@ using UnityEngine.UI;
 using GQ.Client.Conf;
 using System;
 
-/// <summary>
-/// Configures the header layout based on the seetings in the current apps config data. Attach this script to all header game objects.
-/// </summary>
-[RequireComponent (typeof(Image)), RequireComponent (typeof(LayoutElement))]
-public class HeaderButtonLayoutConfig : LayoutConfig {
+namespace GQ.Client.UI {
 
-	protected override void layout() {
-		// set background color:
-		Image bgImage = GetComponent<Image> ();
-		if (bgImage != null) {
-			bgImage.color = ConfigurationManager.Current.headerButtonBgColor;
-		}
+	/// <summary>
+	/// Configures the header layout based on the seetings in the current apps config data. Attach this script to all header game objects.
+	/// </summary>
+	[RequireComponent (typeof(Image)), RequireComponent (typeof(LayoutElement))]
+	public class HeaderButtonLayoutConfig : LayoutConfig {
 
-		// set foreground color in Image:
-		try {
-			Image fgImage = transform.Find("Image").GetComponent<Image> ();
-			if (fgImage != null) {
-				fgImage.color = ConfigurationManager.Current.headerButtonFgColor;
+		protected override void layout() {
+			// set background color:
+			Image bgImage = GetComponent<Image> ();
+			if (bgImage != null) {
+				bgImage.color = ConfigurationManager.Current.headerButtonBgColor;
 			}
-		}
-		catch (Exception) {}	
 
-		// set foreground color as font color in Text:
-		try {
-			Text fgText = transform.Find("Text").GetComponent<Text> ();
-			if (fgText != null) {
-				fgText.color = ConfigurationManager.Current.headerButtonFgColor;
+			// set foreground color in Image:
+			try {
+				Image fgImage = transform.Find("Image").GetComponent<Image> ();
+				if (fgImage != null) {
+					fgImage.color = ConfigurationManager.Current.headerButtonFgColor;
+				}
 			}
+			catch (Exception) {}	
+
+			// set foreground color as font color in Text:
+			try {
+				Text fgText = transform.Find("Text").GetComponent<Text> ();
+				if (fgText != null) {
+					fgText.color = ConfigurationManager.Current.headerButtonFgColor;
+				}
+			}
+			catch (Exception) {}	
 		}
-		catch (Exception) {}	
 	}
+
 }
