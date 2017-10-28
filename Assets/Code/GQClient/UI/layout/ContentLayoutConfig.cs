c8ef4e4ef2e91051bcf34ef4005c05abe3390307
holgerm <mail@holgermuegge.de>
2017-10-28 14:12:29 +0200

    Layout bug on page ImageWithText fixed: Image background color set

--- Assets/Code/GQClient/UI/layout/ContentLayoutConfig.cs
+++ Assets/Code/GQClient/UI/layout/ContentLayoutConfig.cs
@@ -5,26 +5,29 @@ using UnityEngine.UI;
 using GQ.Client.Conf;
 using System;
 
-namespace GQ.Client.UI {
+namespace GQ.Client.UI
+{
 
 	/// <summary>
 	/// Configures the header layout based on the seetings in the current apps config data. Attach this script to all header game objects.
 	/// </summary>
 	[RequireComponent (typeof(Image)), RequireComponent (typeof(LayoutElement))]
-	public class ContentLayoutConfig : LayoutConfig {
+	public class ContentLayoutConfig : LayoutConfig
+	{
 
 		public HeaderLayoutConfig header;
 		public FooterLayoutConfig footer;
 
-		protected override void layout() {
+		protected override void layout ()
+		{
 			// set background color:
-	//		Image image = GetComponent<Image> ();
-	//		if (image != null) {
-	//			image.color = ConfigurationManager.Current.footerBackgroundColor;
-	//		}
-	//
+			Image image = GetComponent<Image> ();
+			if (image != null) {
+				image.color = ConfigurationManager.Current.contentBackgroundColor;
+			}
+	
 			// set height as rest left over by header and footer:
-			LayoutElement layElem = GetComponent<LayoutElement>();
+			LayoutElement layElem = GetComponent<LayoutElement> ();
 			if (layElem != null) {
 				float headerPermill = 0f;
 				if (header != null) {
