e5e1972b62bfa6dfec709766f843b54d1d2004da
holgerm <mail@holgermuegge.de>
2018-01-10 16:04:54 +0100

    warn fix: parent setting uses new Setter now

--- Assets/Code/GQClient/UI/menu/CategoryTreeCtrl.cs
+++ Assets/Code/GQClient/UI/menu/CategoryTreeCtrl.cs
@@ -8,9 +8,11 @@ using GQ.Client.Err;
 using UnityEngine.UI;
 using QM.Util;
 
-namespace GQ.Client.UI {
+namespace GQ.Client.UI
+{
 
-	public class CategoryTreeCtrl : MonoBehaviour {
+	public class CategoryTreeCtrl : MonoBehaviour
+	{
 
 		public Text Title;
 		public Text Number;
@@ -22,12 +24,13 @@ namespace GQ.Client.UI {
 
 
 		// Use this for initialization
-		void Start () {
+		void Start ()
+		{
 			qim = QuestInfoManager.Instance;
 			CategoryFilter = qim.CategoryFilter;
 			qim.OnDataChange += OnQuestInfoChanged;
 		}
-		
+
 		#region React on Events
 
 		public void OnQuestInfoChanged (object sender, QuestInfoChangedEvent e)
@@ -47,7 +50,7 @@ namespace GQ.Client.UI {
 				break;							
 			case ChangeType.ListChanged:
 				// Remove the complete existing category tree and build up a new one based on all quest infos of the quest info manager:
-				UpdateView();
+				UpdateView ();
 				break;							
 			}
 		}
@@ -76,7 +79,8 @@ namespace GQ.Client.UI {
 //			Number.text = ""; // nr.ToString (); TODO make Config?
 		}
 
-		private void recreateModelTree() {
+		private void recreateModelTree ()
+		{
 			// model: create skeleton of folders and entries:
 			categoryEntries = new Dictionary<string, CategoryEntry> ();
 			categoryFolders = new Dictionary<string, CategoryFolder> ();
@@ -86,7 +90,7 @@ namespace GQ.Client.UI {
 				categoryEntries.Add (c.id, catEntry);
 				// Put the new entry in adequate folder and create the folder before if needed
 				CategoryFolder catFolder;
-				if (!categoryFolders.TryGetValue(c.folderName, out catFolder)) {
+				if (!categoryFolders.TryGetValue (c.folderName, out catFolder)) {
 					catFolder = new CategoryFolder (c.folderName);
 					categoryFolders.Add (c.folderName, catFolder);
 				}
@@ -100,7 +104,7 @@ namespace GQ.Client.UI {
 					CategoryEntry catEntry;
 					if (!categoryEntries.TryGetValue (cat, out catEntry)) {
 						// if a quest has an unknown category we skip that:
-						Log.SignalErrorToAuthor("Quest {0} has unkown category {1}.", info.Name, cat);
+						Log.SignalErrorToAuthor ("Quest {0} has unkown category {1}.", info.Name, cat);
 						continue;
 					}
 					// we take note of the catgeory of the current quest in our tree model:
@@ -109,7 +113,8 @@ namespace GQ.Client.UI {
 			}
 		}
 
-		private void recreateUI () {
+		private void recreateUI ()
+		{
 			// delete all category entry UI elements:
 			for (int i = 1; i < transform.childCount; i++) {
 				Transform child = transform.GetChild (i);
@@ -127,7 +132,7 @@ namespace GQ.Client.UI {
 						catFolder: folder,
 						catTree: this
 					);
-				uiFolder.transform.parent = this.transform;
+				uiFolder.transform.SetParent (this.transform);
 				uiFolder.transform.SetAsLastSibling ();
 
 				foreach (CategoryEntry entry in folder.Entries) {
@@ -137,7 +142,7 @@ namespace GQ.Client.UI {
 							catEntry: entry,
 							catTree: this
 						);
-					uiEntry.transform.parent = this.transform;
+					uiEntry.transform.SetParent (this.transform);
 					uiEntry.transform.SetAsLastSibling ();
 				}
 			}
@@ -145,7 +150,8 @@ namespace GQ.Client.UI {
 
 		bool generalSelectionState = true;
 
-		public void SetSelection4AllItems() {
+		public void SetSelection4AllItems ()
+		{
 			generalSelectionState = !generalSelectionState;
 			CategoryFilter.NotificationPaused = true;
 			foreach (CategoryEntry entry in categoryEntries.Values) {
@@ -154,13 +160,12 @@ namespace GQ.Client.UI {
 			CategoryFilter.NotificationPaused = false;
 
 			if (generalSelectionState) {
-				OnOff.color = new Color(OnOff.color.r, OnOff.color.g, OnOff.color.b, 1f);
-			}
-			else {
-				OnOff.color = new Color(OnOff.color.r, OnOff.color.g, OnOff.color.b, ConfigurationManager.Current.disabledAlpha);
+				OnOff.color = new Color (OnOff.color.r, OnOff.color.g, OnOff.color.b, 1f);
+			} else {
+				OnOff.color = new Color (OnOff.color.r, OnOff.color.g, OnOff.color.b, ConfigurationManager.Current.disabledAlpha);
 			}
 		}
-			
+
 		#endregion
 
 
@@ -168,21 +173,25 @@ namespace GQ.Client.UI {
 
 		public Dictionary<string, CategoryFolder> categoryFolders;
 
-		public class CategoryFolder {
+		public class CategoryFolder
+		{
 			public string Name;
 
 			public List<CategoryEntry> Entries;
 
-			public CategoryFolder(string name) {
+			public CategoryFolder (string name)
+			{
 				Name = name;
-				Entries = new List<CategoryEntry>();
+				Entries = new List<CategoryEntry> ();
 			}
 
-			public void AddCategoryEntry(CategoryEntry entry) {
+			public void AddCategoryEntry (CategoryEntry entry)
+			{
 				Entries.Add (entry);
 			}
 
-			public void RemoveCategoryEntry(CategoryEntry entry) {
+			public void RemoveCategoryEntry (CategoryEntry entry)
+			{
 				Entries.Remove (entry);
 			}
 
@@ -190,7 +199,8 @@ namespace GQ.Client.UI {
 			/// Returns the number of quests currently represented by this category.
 			/// </summary>
 			/// <returns>The of quests.</returns>
-			public int NumberOfQuests() {
+			public int NumberOfQuests ()
+			{
 				int nr = 0;
 				foreach (CategoryEntry catEntry in Entries) {
 					nr += catEntry.NumberOfQuests ();
@@ -201,16 +211,18 @@ namespace GQ.Client.UI {
 
 		protected Dictionary<string, CategoryEntry> categoryEntries;
 
-		public class CategoryEntry {
+		public class CategoryEntry
+		{
 			public Category category;
 			List<int> questIds;
 
 			/// Will be set by the ui controller, when it is created from prefab, c.f. the Create() function in the CategoryEntryCtrl class.
 			public CategoryEntryCtrl ctrl;
 
-			public CategoryEntry(Category category) {
+			public CategoryEntry (Category category)
+			{
 				this.category = category;
-				this.questIds = new List<int>();
+				this.questIds = new List<int> ();
 				this.ctrl = null;
 			}
 
@@ -218,8 +230,9 @@ namespace GQ.Client.UI {
 			/// Adds a quest to be represented by this category entry (without duplicates).
 			/// </summary>
 			/// <param name="questID">Quest I.</param>
-			public void AddQuestID(int questID) {
-				if (!questIds.Contains(questID)) {
+			public void AddQuestID (int questID)
+			{
+				if (!questIds.Contains (questID)) {
 					questIds.Add (questID);
 				}
 			}
@@ -228,8 +241,9 @@ namespace GQ.Client.UI {
 			/// Removes a quest as being respresented by this catgeory:
 			/// </summary>
 			/// <param name="questID">Quest I.</param>
-			public void RemoveQuestID(int questID) {
-				if (questIds.Contains(questID)) {
+			public void RemoveQuestID (int questID)
+			{
+				if (questIds.Contains (questID)) {
 					questIds.Remove (questID);
 				}
 			}
@@ -238,7 +252,8 @@ namespace GQ.Client.UI {
 			/// Returns the number of quests currently represented by this category.
 			/// </summary>
 			/// <returns>The of quests.</returns>
-			public int NumberOfQuests() {
+			public int NumberOfQuests ()
+			{
 				return questIds.Count;
 			}
 		}
