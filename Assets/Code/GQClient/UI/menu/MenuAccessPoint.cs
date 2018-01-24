956126977f6edd8b89880367df04565541cc9b3e
Holger Muegge <mail@holgermuegge.de>
2018-01-24 08:45:54 +0100

    menu access point (first step towards configurable menus)

---
+++ Assets/Code/GQClient/UI/menu/MenuAccessPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GQ.Client.UI {
+
+	public class MenuAccessPoint : MonoBehaviour {
+
+		public GameObject MenuTopLeftContent;
+
+	}
+}
