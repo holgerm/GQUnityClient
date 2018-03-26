5733652a6ad88c3b41a4a80db4b1bec40feb1d2c
holgerm <mail@holgermuegge.de>
2018-03-26 07:35:08 +0200

    scripts endquest

---
+++ Assets/Code/GQClient/Scripts/EndQuest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GQ.Client.Model;
+
+namespace GQ.Client.Scripts
+{
+
+	public class EndQuest : MonoBehaviour
+	{
+
+		public void DoEndQuest ()
+		{
+			QuestManager.Instance.CurrentQuest.End ();
+		}
+
+	}
+}
