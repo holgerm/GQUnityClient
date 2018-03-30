6a98cfc116eb7684eab9a65a79f00f587d7a253f
holgerm <mail@holgermuegge.de>
2018-03-30 23:40:58 +0200

    prod slsspiele

--- Production/products/slsspiele/Code/CategoryReader.cs
+++ Production/products/slsspiele/Code/CategoryReader.cs
@@ -4,37 +4,40 @@ using UnityEngine;
 using GQ.Client.Model;
 using GQ.Client.Util;
 
-namespace GQ.Client.Model {
+namespace GQ.Client.Model
+{
 
-	 public class CategoryReader
-	 {
+	public class CategoryReader
+	{
 
-		  public static List<string> ReadCategoriesFromMetadata (MetaDataInfo[] metadata)
-		  {
+		public static List<string> ReadCategoriesFromMetadata (MetaDataInfo[] metadata)
+		{
 			
-				List<string> categories = new List<string>();
-				string netVal;
-				foreach ( MetaDataInfo md in metadata ) {
-					 switch ( md.Key ) {
-						  case "Spieltyp1":
-								netVal = md.Value.StripQuotes();
-								if ( netVal != "" )
-									 categories.Insert(0, netVal);
-								break;
-						  case "Spieltyp2":
-						  case "Spieltyp3":
-						  case "Spieleranzahl":
-						  case "Dauer":
-						  case "Wetter":
-						  case "Orte":
-								netVal = md.Value.StripQuotes();
-								if ( netVal != "" )
-									 categories.Add(netVal);
-								break;
-					 }
+			List<string> categories = new List<string> ();
+			string netVal;
+			foreach (MetaDataInfo md in metadata) {
+				switch (md.Key) {
+				case "Spieltyp1":
+					netVal = md.Value.StripQuotes ();
+					if (netVal != "")
+						categories.Insert (0, netVal);
+					break;
+				case "Spieltyp2":
+				case "Spieltyp3":
+				case "Stichworte":
+				case "Weitere Stichworte":
+				case "Spieleranzahl":
+				case "Dauer":
+				case "Wetter":
+				case "Orte":
+					netVal = md.Value.StripQuotes ();
+					if (netVal != "")
+						categories.Add (netVal);
+					break;
 				}
-				return categories;
-		  }
-	 }
+			}
+			return categories;
+		}
+	}
 
 }
--- Production/products/slsspiele/Code/SLS_PageController.cs
+++ Production/products/slsspiele/Code/SLS_PageController.cs
@@ -1,12 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GQ.Client.UI
 {
 
-	public class SLS_PageController : NPCTalkController {
-		
+	public class SLS_PageController : NPCTalkController
+	{
+	
+		#region Inspector Fields
+
+		public Text titleText;
+
+		#endregion
+
+		#region Runtime API
+
+		/// <summary>
+		/// Is called during Start() of the base class, which is a MonoBehaviour.
+		/// </summary>
+		public override void Initialize ()
+		{
+			base.Initialize ();
+
+			titleText.text = page.Quest.Name;
+		}
+
+		#endregion
+
 	}
 
 }
