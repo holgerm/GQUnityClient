ff139d7f8aae0209013bcc8fa15a712beb2746f3
holgerm <mail@holgermuegge.de>
2018-05-15 23:32:39 +0200

    new MultipleChoiceQUestion Page basic controller

---
+++ Assets/Code/GQClient/UI/pages/multiplechoicequestion/MultipleChoiceQuestionController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GQ.Client.Model;
+
+namespace GQ.Client.UI
+{
+
+	public class MultipleChoiceQuestionController : PageController
+	{
+
+		#region Runtime API
+
+		protected PageMultipleChoiceQuestion mcqPage;
+
+		/// <summary>
+		/// Is called during Start() of the base class, which is a MonoBehaviour.
+		/// </summary>
+		public override void Initialize ()
+		{
+			mcqPage = (PageMultipleChoiceQuestion)page;
+
+			// show the content:
+//			ShowImage ();
+//			ClearText ();
+//			AddCurrentText ();
+//			UpdateForwardButton ();
+		}
+
+		#endregion
+
+	}
+}
