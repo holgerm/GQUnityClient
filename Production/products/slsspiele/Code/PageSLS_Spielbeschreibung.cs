7073e8b2c7a841e220d400952958db9fbe0897cb
holgerm <mail@holgermuegge.de>
2018-05-17 10:10:36 +0200

    prod slsspiele

--- Production/products/slsspiele/Code/PageSLS_Spielbeschreibung.cs
+++ Production/products/slsspiele/Code/PageSLS_Spielbeschreibung.cs
@@ -5,11 +5,62 @@ using System.Xml.Serialization;
 using System;
 using System.Xml;
 
-namespace GQ.Client.Model {
-	
+namespace GQ.Client.Model
+{
+
+	/// <summary>
+	/// SLS Quests have a main page that relies on the MetaData page. Hence, its keys and values are stored in the globally accessible dictionary 
+	/// QuestManager.CurrentlyParsingQuest.metadata<string, string>.
+	/// 
+	/// Thus, the model here is pretty simple.
+	/// 
+	/// The UI Controller does the whiole work using the global metadata dictionary.
+	/// </summary>
 	[XmlRoot (GQML.PAGE)]
-	public class PageSLS_Spielbeschreibung : PageNPCTalk
+	public class PageSLS_Spielbeschreibung : PageMetaData
 	{
+
+
+		#region XML Serialization
+
+		protected override void ReadContent (XmlReader reader, XmlRootAttribute xmlRootAttr)
+		{
+			switch (reader.LocalName) {
+			case GQML.PAGE_METADATA_STRINGMETA:
+				xmlRootAttr.ElementName = GQML.PAGE_METADATA_STRINGMETA;
+				XmlSerializer serializer = new XmlSerializer (typeof(StringMetaData), xmlRootAttr);
+				StringMetaData smde = (StringMetaData)serializer.Deserialize (reader);
+				if (smde.Key == null)
+					break;
+				if (!QuestManager.CurrentlyParsingQuest.metadata.ContainsKey (smde.Key)) {
+					QuestManager.CurrentlyParsingQuest.metadata.Add (smde.Key, smde.Value);
+					if (isMedia (smde)) {
+						QuestManager.CurrentlyParsingQuest.AddMedia (smde.Value);
+					}
+				}
+				break;
+			default:
+				base.ReadContent (reader, xmlRootAttr);
+				break;
+			}
+		}
+
+		bool isMedia (StringMetaData smd)
+		{
+			return ("Bild".Equals (smd.Key));
+		}
+
+		#endregion
+
+
+		#region Runtime API
+
+		public override bool CanStart ()
+		{
+			return true;
+		}
+
+		#endregion
 
 
 	}
--- Production/products/slsspiele/Code/SLS_PageController.cs
+++ Production/products/slsspiele/Code/SLS_PageController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using GQ.Client.Model;
 
 namespace GQ.Client.UI
 {
 
-	public class SLS_PageController : NPCTalkController
+	public class SLS_PageController : PageController
 	{
 	
 		#region Inspector Fields
@@ -17,13 +18,16 @@ namespace GQ.Client.UI
 
 		#region Runtime API
 
+		PageSLS_Spielbeschreibung slsPage;
+
 		/// <summary>
 		/// Is called during Start() of the base class, which is a MonoBehaviour.
 		/// </summary>
 		public override void Initialize ()
 		{
-			base.Initialize ();
+			slsPage = (PageSLS_Spielbeschreibung)page;
 
+			// set title:
 			titleText.text = page.Quest.Name;
 		}
 
