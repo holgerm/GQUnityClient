d56cb4b06e26216cdf87449892cb5ed08b1232a0
Holger Muegge <mail@holgermuegge.de>
2018-06-05 23:19:11 +0200

    new menu control

--- Assets/Code/GQClient/UI/menu/MenuAccessPoint.cs
+++ Assets/Code/GQClient/UI/menu/MenuAccessPoint.cs
@@ -9,7 +9,54 @@ namespace GQ.Client.UI
 	{
 
 		public GameObject MenuTopLeftContent;
+		public Canvas canvas4TopLeftMenu;
 		public GameObject MenuTopRightContent;
+		public Canvas canvas4TopRightMenu;
 
+		public Canvas imprintCanvas;
+		public Canvas privacyCanvas;
+		public Canvas authorCanvas;
+
+		public void LeftMenuButtonPressed() {
+			imprintCanvas.gameObject.SetActive (false);
+			privacyCanvas.gameObject.SetActive (false);
+			authorCanvas.gameObject.SetActive (false);
+
+			canvas4TopLeftMenu.gameObject.SetActive (!canvas4TopLeftMenu.gameObject.activeSelf);
+
+			if (canvas4TopLeftMenu.gameObject.activeSelf)
+				canvas4TopRightMenu.gameObject.SetActive (false);
+		}
+
+		public void RightMenuButtonPressed() {
+			if (imprintCanvas.gameObject.activeSelf) {
+				imprintCanvas.gameObject.SetActive (false);
+				return;
+			}
+
+			if (privacyCanvas.gameObject.activeSelf) {
+				privacyCanvas.gameObject.SetActive (false);
+				return;
+			}
+
+			if (authorCanvas.gameObject.activeSelf) {
+				authorCanvas.gameObject.SetActive (false);
+				return;
+			}
+
+			canvas4TopRightMenu.gameObject.SetActive (!canvas4TopRightMenu.gameObject.activeSelf);
+
+			if (canvas4TopRightMenu.gameObject.activeSelf)
+				canvas4TopLeftMenu.gameObject.SetActive (false);
+
+		}
+
+		public void CloseLeftMenu() {
+			canvas4TopLeftMenu.gameObject.SetActive (false);
+		}
+
+		public void CloseRightMenu() {
+			canvas4TopRightMenu.gameObject.SetActive (false);
+		}
 	}
 }
