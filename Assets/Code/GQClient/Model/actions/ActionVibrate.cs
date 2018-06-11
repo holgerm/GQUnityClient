fb908c1bdcee7537a79a7a12420e84eb4801fdf6
Holger Muegge <mail@holgermuegge.de>
2018-06-11 20:38:34 +0200

    new action Vibrate

---
+++ Assets/Code/GQClient/Model/actions/ActionVibrate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GQ.Client.Model
+{
+
+	public class ActionVibrate : ActionAbstract {
+
+		public override void Execute ()
+		{
+			Handheld.Vibrate ();
+		}
+
+
+	}
+}
