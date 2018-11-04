5e06024a1ee958008f3f1ecbc17fb1da2cb3a0ea
Holger Muegge <mail@holgermuegge.de>
2018-11-04 12:22:48 +0100

    prod backup ebk

---
+++ Production/products/ebk-old/Code/CategoryReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GQ.Client.Util;
+
+namespace GQ.Client.Model {
+
+	public class CategoryReader {
+
+		public static List<string> ReadCategoriesFromMetadata(MetaDataInfo[] metadata) {
+			
+			List<string> categories = new List<string> ();
+			string netVal;
+			foreach (MetaDataInfo md in metadata) {
+				switch (md.Key) {
+				case "category":
+				case "category1":
+					netVal = md.Value.StripQuotes ();
+					if (netVal != "")
+						categories.Insert (0, netVal);
+					break;
+				case "category2":
+					netVal = md.Value.StripQuotes ();
+					if (netVal != "")
+						categories.Add (netVal);
+					break;
+				}
+			}
+			return categories;
+		}
+	}
+
+}
