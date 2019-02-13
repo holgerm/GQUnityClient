b97fde18efcc603bb1d5e84373daf8f9d44f0ea5
Holger Muegge <mail@holgermuegge.de>
2019-02-13 21:25:34 +0100

    prod ebk new categories

--- Production/products/ebk/Code/CategoryReader.cs
+++ Production/products/ebk/Code/CategoryReader.cs
@@ -1,33 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
 using GQ.Client.Util;
 
-namespace GQ.Client.Model {
+namespace GQ.Client.Model
+{
 
-	public class CategoryReader {
+    public class CategoryReader
+    {
 
-		public static List<string> ReadCategoriesFromMetadata(MetaDataInfo[] metadata) {
-			
-			List<string> categories = new List<string> ();
-			string netVal;
-			foreach (MetaDataInfo md in metadata) {
-				switch (md.Key) {
-				case "category":
-				case "category1":
-					netVal = md.Value.StripQuotes ();
-					if (netVal != "")
-						categories.Insert (0, netVal);
-					break;
-				case "category2":
-					netVal = md.Value.StripQuotes ();
-					if (netVal != "")
-						categories.Add (netVal);
-					break;
-				}
-			}
-			return categories;
-		}
-	}
+        public static List<string> ReadCategoriesFromMetadata(MetaDataInfo[] metadata)
+        {
+
+            List<string> categories = new List<string>();
+            string netVal;
+            foreach (MetaDataInfo md in metadata)
+            {
+                switch (md.Key)
+                {
+                    case "category":
+                    case "category1":
+                        netVal = md.Value.StripQuotes();
+                        if (netVal != "")
+                            categories.Insert(0, netVal);
+                        break;
+                    case "category2":
+                    case "category3":
+                    case "category4":
+                        netVal = md.Value.StripQuotes();
+                        if (netVal != "")
+                            categories.Add(netVal);
+                        break;
+                }
+            }
+            return categories;
+        }
+    }
 
 }
