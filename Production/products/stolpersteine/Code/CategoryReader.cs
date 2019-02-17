0c44111ad4bb51aea99bce243c03fd79c16dff08
Holger Muegge <mail@holgermuegge.de>
2019-02-17 17:41:11 +0100

    new prod stolpersteine

---
+++ Production/products/stolpersteine/Code/CategoryReader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using GQ.Client.Conf;
+using GQ.Client.Util;
+
+namespace GQ.Client.Model
+{
+
+    public class CategoryReader
+    {
+
+        public static List<string> ReadCategoriesFromMetadata(MetaDataInfo[] metadata)
+        {
+
+            List<string> categories = new List<string>();
+            string netVal;
+            foreach (MetaDataInfo md in metadata)
+            {
+                switch (md.Key)
+                {
+                    case "category":
+                    case "category1":
+                        netVal = md.Value.StripQuotes();
+                        if (netVal != "")
+                            categories.Insert(0, netVal);
+                        break;
+                    case "category2":
+                    case "category3":
+                    case "category4":
+                        netVal = md.Value.StripQuotes();
+                        if (netVal != "")
+                            categories.Add(netVal);
+                        break;
+                }
+            }
+            if (categories.Count == 0 && ConfigurationManager.Current.defaultCategory != null)
+            {
+                categories.Add(ConfigurationManager.Current.defaultCategory);
+            }
+            return categories;
+        }
+    }
+
+}
