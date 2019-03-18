25fc74ba62ccd70165bbc698f4d9487c2ea53f27
Holger Muegge <mail@holgermuegge.de>
2019-03-18 08:08:07 +0100

    bugfix page ImageWithText text occurred twice

--- Assets/Code/GQClient/Model/pages/PageImageWithText.cs
+++ Assets/Code/GQClient/Model/pages/PageImageWithText.cs
@@ -13,39 +13,11 @@ namespace GQ.Client.Model
 	public class PageImageWithText : PageNPCTalk
 	{
 
-		#region Runtime API
-
-		public override void Start (bool canReturnToPrevious = false)
-		{
-			base.Start (canReturnToPrevious);
-		}
-
 		public override string PageSceneName {
 			get {
 				return GQML.PAGE_TYPE_NPCTALK;
 			}
 		}
-
-		#endregion
-
-
-		#region XML Serialization
-
-		protected override void ReadAttributes (XmlReader reader)
-		{
-			base.ReadAttributes (reader);
-
-			EndButtonText = GQML.GetStringAttribute (GQML.PAGE_IMAGEWITHTEXT_ENDBUTTONTEXT, reader);
-
-			ImageUrl = GQML.GetStringAttribute (GQML.PAGE_IMAGEWITHTEXT_IMAGEURL, reader);
-			QuestManager.CurrentlyParsingQuest.AddMedia (ImageUrl, "ImageWithText." + GQML.PAGE_IMAGEWITHTEXT_IMAGEURL);
-
-			Text = GQML.GetStringAttribute (GQML.PAGE_IMAGEWITHTEXT_TEXT, reader);
-
-			TextSize = GQML.GetIntAttribute (GQML.PAGE_IMAGEWITHTEXT_TEXTSIZE, reader);
-		}
-
-		#endregion
 
 	}
 
