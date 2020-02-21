a92a655b95a61823498d3f4d664d1435e4f097bd
Holger Muegge <mail@holgermuegge.de>
2020-02-21 18:50:24 +0100

    new canvas fade out component

---
+++ Assets/Code/QM/Util/FadeOutOnFirstUse.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Code.QM.Util
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class FadeOutOnFirstUse : MonoBehaviour
+    {
+        private static bool _done;
+
+        public float timeToShow = 2;
+        public float timeToFade = 1.3f;
+        public CanvasGroup canvasGroup;
+
+        public void Reset()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        // Start is called before the first frame update
+        private IEnumerator Start()
+        {
+            if (_done)
+            {
+                yield break;
+            }
+
+            if (canvasGroup == null)
+            {
+                canvasGroup = GetComponent<CanvasGroup>();
+            }
+
+            var timeSinceStart = 0f;
+
+            while (timeSinceStart < timeToShow + timeToFade)
+            {
+                timeSinceStart += Time.deltaTime;
+                if (canvasGroup != null && timeSinceStart > timeToShow)
+                {
+                    canvasGroup.alpha =
+                        Mathf.Lerp(1f, 0f, (timeSinceStart - timeToShow) / timeToFade);
+                }
+
+                yield return null;
+            }
+
+            gameObject.SetActive(false); // TODO fade out?
+            if (canvasGroup != null)
+                canvasGroup.alpha = 1;
+            _done = true;
+        }
+    }
+}
