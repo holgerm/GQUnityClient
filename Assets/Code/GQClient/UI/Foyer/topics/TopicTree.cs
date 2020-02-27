0b3a7f59cbfb399f0b54264a460f170cf96d54a7
Holger Muegge <mail@holgermuegge.de>
2020-02-27 11:23:41 +0100

    empty topic tree class

---
+++ Assets/Code/GQClient/UI/Foyer/topics/TopicTree.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.GQClient.UI.Foyer
+{
+    public class TopicTree
+    {
+        
+        public void AddTopicPath(string newTopicPath)
+        {
+            
+        }
+        
+        public void RemoveTopicPath(string newTopicPath)
+        {
+            
+        }
+
+    }
+}
