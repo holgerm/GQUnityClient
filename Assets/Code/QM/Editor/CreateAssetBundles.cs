e4c5cfdcc42f3a183919c12a616d86d2cc61d615
Holger Muegge <mail@holgermuegge.de>
2020-03-30 11:50:15 +0200

    automatically rebuild asset bundles when switching the platform

--- Assets/Code/QM/Editor/CreateAssetBundles.cs
+++ Assets/Code/QM/Editor/CreateAssetBundles.cs
@@ -1,19 +1,27 @@
 ﻿using UnityEditor;
 using System.IO;
+using UnityEditor.Build;
 using UnityEngine;
 
 namespace QM.EditUtils
 {
-    public class CreateAssetBundles
+    public class CreateAssetBundles : IActiveBuildTargetChanged
     {
         [MenuItem("Assets/Build AssetBundles")]
-        static void BuildAllAssetBundles4Android()
+        static void BuildAllAssetBundles()
         {
             BuildPipeline.BuildAssetBundles(
                 Path.Combine(Application.streamingAssetsPath),
                 BuildAssetBundleOptions.None,
                 EditorUserBuildSettings.activeBuildTarget);
             Debug.Log("Asset bundles built for " + EditorUserBuildSettings.activeBuildTarget.ToString());
+        }
+
+        public int callbackOrder => 0;
+        
+        public void OnActiveBuildTargetChanged(BuildTarget previousTarget, BuildTarget newTarget)
+        {
+            BuildAllAssetBundles();
         }
     }
 }
