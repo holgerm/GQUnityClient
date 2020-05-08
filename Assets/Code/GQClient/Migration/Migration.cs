b828b36898c368682beb6ddca8f24a61c854ea4b
Holger Muegge <mail@holgermuegge.de>
2020-05-08 13:00:29 +0200

    methods for migration moved as static methods to Migration class

---
+++ Assets/Code/GQClient/Migration/Migration.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Code.GQClient.Err;
+using Code.GQClient.Model;
+using Code.GQClient.Util.tasks;
+using UnityEngine;
+
+namespace Code.GQClient.Migration
+{
+    public abstract class Migration : Task
+    {
+        protected abstract string Version { get; }
+        
+        public static string Title  => "Aktualisierung der App";
+        
+        public abstract string Details { get; }
+
+        private bool Applicable => string.CompareOrdinal(BuildTimeText, Version) < 0;
+
+        private static int LowerVersionsFirst(Migration x, Migration y)
+        {
+            return string.CompareOrdinal(x.Version, y.Version);
+        }
+
+        private static void ApplyMigrations()
+        {
+            var assembly = typeof(Migration).Assembly;
+            var types = assembly.GetTypes();
+            var migrations = new List<Migration>();
+            
+            foreach (var type in types)
+            {
+                if (type.IsSubclassOf(typeof(Migration)))
+                {
+                    if (Activator.CreateInstance(type) is Migration migration)
+                    {
+                        //   var _ = new SimpleDialogBehaviour(migration, Migration.Title, migration.Details);
+                        if (migration.Applicable)
+                            migrations.Add(migration);
+                    }
+                    else
+                    {
+                        Log.SignalErrorToDeveloper($"Migration {type.FullName} missing default constructor.");
+                    }
+                }
+            }
+            
+            migrations.Sort(Migration.LowerVersionsFirst);
+            foreach (var migration in migrations)
+            {
+                migration.Start();
+            }
+        }
+
+        private static string _buildTimeText;
+
+        public static string BuildTimeText
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(_buildTimeText))
+                    return _buildTimeText;
+
+                var buildtimeTextAsset = Resources.Load<TextAsset>("buildtime");
+                if (buildtimeTextAsset == null)
+                {
+                    Log.SignalErrorToDeveloper("Buildtime not found.");
+                    return "";
+                }
+
+                _buildTimeText = buildtimeTextAsset.text;
+                // just use the version number, e.g. "2.20.05.05 (05.05.2020 09:36:27)" ==> "2.20.05.05"
+                _buildTimeText = _buildTimeText.Substring(
+                    0, _buildTimeText.IndexOf(" (", StringComparison.Ordinal));
+
+                var oldBuildTime = PlayerPrefs.HasKey(Definitions.GQ_PLAYERPREF_BUILDTIME)
+                    ? PlayerPrefs.GetString(Definitions.GQ_PLAYERPREF_BUILDTIME)
+                    : "0.0.0.0";
+                if (string.CompareOrdinal(oldBuildTime, _buildTimeText) < 0)
+                {
+                    Migration.ApplyMigrations();
+                }
+
+                PlayerPrefs.SetString(Definitions.GQ_PLAYERPREF_BUILDTIME, _buildTimeText);
+                PlayerPrefs.Save();
+                return _buildTimeText;
+            }
+        }
+    }
+    
+}
---
+++ Assets/Code/GQClient/Migration/UpdateToCentralMediaStore.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using Code.GQClient.Err;
+using Code.GQClient.FileIO;
+using Code.GQClient.Model.mgmt.quests;
+using GQClient.Model;
+using Newtonsoft.Json;
+
+namespace Code.GQClient.Migration
+{
+    // ReSharper disable once UnusedType.Global
+    public class UpdateToCentralMediaStore : Migration
+    {
+        protected override string Version => "2.20.05.07";
+        public override string Details => "Wir müssen etwas aufräumen für die neue App Version. Das dauert ein wenig.";
+
+        protected override IEnumerator DoTheWork()
+        {
+            var globalFilesDirPath = Files.CombinePath(
+                QuestInfoManager.LocalQuestsPath,
+                "files");
+
+            var migratedQuests = 0;
+
+            // go through each quest folder and deserialize the game.xml
+            foreach (var questPath in Directory.GetDirectories(QuestInfoManager.LocalQuestsPath))
+            {
+                var gameFilePath = Files.CombinePath(questPath, "game.xml");
+                var questFilesDirPath = Files.CombinePath(questPath, "files");
+                if (!File.Exists(gameFilePath) || !Directory.Exists(questFilesDirPath) ||
+                    Files.IsEmptyDir(questFilesDirPath))
+                    continue; // skip dirs without a game inside
+
+                var gameXml = File.ReadAllText(gameFilePath);
+
+                yield return null;
+
+                var quest = QuestManager.DeserializeQuest(gameXml);
+
+                yield return null;
+
+                foreach (var mediaInfo in quest.MediaStore.Values)
+                {
+                    var oldFileName = mediaInfo.LocalFileName;
+                    var newFileName = QuestManager.Instance.IncreaseMediaUsage(mediaInfo);
+                    if (newFileName == null)
+                        continue;
+
+                    // copy media file to global file directory under the new name
+                    var sourceFilePath = Files.CombinePath(questFilesDirPath, oldFileName);
+                    if (File.Exists(sourceFilePath))
+                    {
+                        File.Copy(sourceFilePath, Files.CombinePath(globalFilesDirPath, newFileName));
+                        yield return null;
+                    }
+                }
+
+                Files.DeleteDirCompletely(questFilesDirPath);
+                migratedQuests++;
+            }
+
+            Log.InformDeveloper($"Migrated using {GetType().Name} {migratedQuests} quests to version {Version}");
+
+            //     last. in media.json delete all dir parts for each entry  
+            var mediaList = QuestManager.Instance.GetListOfGlobalMediaInfos();
+
+            try
+            {
+                var mediaJson =
+                    (mediaList.Count == 0)
+                        ? "[]"
+                        : JsonConvert.SerializeObject(mediaList, Newtonsoft.Json.Formatting.Indented);
+                Files.WriteAllText(QuestManager.GlobalMediaJsonPath, mediaJson);
+                QuestManager.Instance.MediaStoreIsDirty = false;
+            }
+            catch (Exception e)
+            {
+                Log.SignalErrorToDeveloper("Error while trying to export quest info json file: " + e.Message);
+                RaiseTaskFailed();
+            }
+        }
+    }
+}
