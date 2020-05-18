1aca27fab135172173904c8a5a63df2e38835f57
Holger Muegge <mail@holgermuegge.de>
2020-05-18 23:42:51 +0200

    startcanvascontroller moved from importpackage to fix code base of GQ

---
+++ Assets/Code/GQClient/start/StartCanvasController.cs
@@ -0,0 +1,40 @@
+﻿// #define DEBUG_LOG
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Code.GQClient.start
+{
+    public class StartCanvasController : MonoBehaviour
+    {
+        public Image fadeInImage;
+        public float waitTimeBeforeFadeIn;
+        public float fadeInTime;
+        public float waitTimeAfterFadeIn;
+       
+                
+        // Start is called before the first frame update
+        private IEnumerator Start()
+        {
+            if (fadeInImage != null)
+            {
+                fadeInImage.color = new Color(1f, 1f, 1f, 0f);
+
+                yield return new WaitForSeconds(waitTimeBeforeFadeIn);
+
+                for (var t = 0.0f; t < 1.0f; t += Time.deltaTime / fadeInTime)
+                {
+                    var newColor = new Color(1, 1, 1, Mathf.Lerp(0.0f, 1.0f, t));
+                    fadeInImage.color = newColor;
+                    yield return null;
+                }
+            }
+
+            yield return new WaitForSeconds(waitTimeAfterFadeIn);
+
+            SceneManager.LoadScene("Foyer");
+        }
+    }
+}
